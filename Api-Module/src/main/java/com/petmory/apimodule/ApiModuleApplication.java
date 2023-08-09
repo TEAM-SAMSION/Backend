611118d89f75856804cea9c527bce4254021c303
tlarbals824 <simkm0824@naver.com>
2023-08-09 10:39:47 +0900

    PET-44 feat : UserDetailsServiceAutoConfiguration.class 설정되지 않게 설정

--- Api-Module/src/main/java/com/petmory/apimodule/ApiModuleApplication.java
+++ Api-Module/src/main/java/com/petmory/apimodule/ApiModuleApplication.java
@@ -3,9 +3,10 @@ package com.petmory.apimodule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class })
 @ComponentScan(basePackages = {"com.petmory"})
 @EntityScan(basePackages = {"com.petmory"})
 public class ApiModuleApplication {
