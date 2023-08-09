c7853e19a40ac4786ea4ebb6f39c5cd07a684cd7
tlarbals824 <simkm0824@naver.com>
2023-08-09 16:27:50 +0900

    PET-41 feat : ErrorCode 반환 Response 클래스 추가

---
+++ Common-Module/src/main/java/com/petmory/commonmodule/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.petmory.commonmodule.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private final int errorCode;
+
+    private ErrorResponse(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public static ErrorResponse from(BusinessException exception){
+        return new ErrorResponse(exception.getErrorCode());
+    }
+}
