dfa4693d34b788a5c5f48417da0fb391154d7d3b
tlarbals824 <simkm0824@naver.com>
2023-08-19 19:19:23 +0900

    PET-69 feat : 소셜 로그인 결과 사용자 email 기반 AccessToken, RefreshToken 생성 로직 추가

--- Auth-Module/auth-application/src/main/java/com/pawith/authmodule/application/port/out/observer/subject/AbstractOAuthSubject.java
+++ Auth-Module/auth-application/src/main/java/com/pawith/authmodule/application/port/out/observer/subject/AbstractOAuthSubject.java
@@ -1,5 +1,6 @@
 package com.pawith.authmodule.application.port.out.observer.subject;
 
+import com.pawith.authmodule.application.common.consts.AuthConsts;
 import com.pawith.authmodule.application.dto.OAuthRequest;
 import com.pawith.authmodule.application.dto.OAuthResponse;
 import com.pawith.authmodule.application.dto.OAuthUserInfo;
@@ -7,17 +8,22 @@ import com.pawith.authmodule.application.port.out.observer.observer.AbstractOAut
 import com.pawith.commonmodule.observer.observer.Observer;
 import com.pawith.commonmodule.observer.subject.Status;
 import com.pawith.commonmodule.observer.subject.Subject;
-import lombok.NoArgsConstructor;
+import com.pawith.jwt.JWTProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 @Slf4j
-@NoArgsConstructor
 public final class AbstractOAuthSubject implements Subject<OAuthRequest, OAuthResponse> {
+
+    private static final String JWT_PROVIDER_BEAN_NAME = "JWTProvider";
+
     private final List<AbstractOAuthObserver> observerList = new ArrayList<>();
+
+    private JWTProvider jwtProvider;
 
     @Override
     public void registerObserver(Observer<? extends Status,?> o) {
@@ -32,7 +38,7 @@ public final class AbstractOAuthSubject implements Subject<OAuthRequest, OAuthRe
     @Override
     public OAuthResponse notifyObservers(OAuthRequest object) {
         final OAuthUserInfo oAuthUserInfo = attemptLogin(new OAuth(object.getProvider(), object.getAccessToken()));
-        return generateServerAccessTool(oAuthUserInfo);
+        return generateServerAuthenticationTokens(oAuthUserInfo);
     }
 
     private OAuthUserInfo attemptLogin(OAuth oAuth) {
@@ -44,13 +50,30 @@ public final class AbstractOAuthSubject implements Subject<OAuthRequest, OAuthRe
         throw new IllegalArgumentException("로그인 실패");
     }
 
-    private OAuthResponse generateServerAccessTool(OAuthUserInfo oAuthUserInfo) {
-        return new OAuthResponse(oAuthUserInfo.getEmail()+" "+oAuthUserInfo.getUsername());
+    private OAuthResponse generateServerAuthenticationTokens(OAuthUserInfo oAuthUserInfo) {
+        final String accessToken = attachAuthenticationType(jwtProvider::generateAccessToken, oAuthUserInfo.getEmail());
+        final String refreshToken = attachAuthenticationType(jwtProvider::generateRefreshToken, oAuthUserInfo.getEmail());
+        return OAuthResponse.builder()
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .build();
+    }
+
+    private <T> String attachAuthenticationType(Function<T, String> generateTokenMethod, T includeClaimData) {
+        return AuthConsts.AUTHENTICATION_TYPE + generateTokenMethod.apply(includeClaimData);
     }
 
     public void initObserver(ApplicationContext applicationContext){
+        initOAuthObserver(applicationContext);
+        initJWTProvider(applicationContext);
+    }
+
+    private void initOAuthObserver(ApplicationContext applicationContext) {
         applicationContext.getBeansOfType(AbstractOAuthObserver.class)
                 .values()
                 .forEach(this::registerObserver);
+    }
+    private void initJWTProvider(ApplicationContext applicationContext){
+        jwtProvider = applicationContext.getBean(JWT_PROVIDER_BEAN_NAME, JWTProvider.class);
     }
 }
