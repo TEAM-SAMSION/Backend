329181d541414c4dc9c95d6895968888bd1d89c6
tlarbals824 <simkm0824@naver.com>
2023-08-22 20:41:41 +0900

    PET-73 refactor : Observer 기반 OAuth 삭제

--- Auth-Module/auth-application/src/main/java/com/pawith/authmodule/application/port/out/factory/OAuthFactory.java
+++
@@ -1,9 +0,0 @@
-package com.pawith.authmodule.application.port.out.factory;
-
-
-import com.pawith.authmodule.application.dto.OAuthResponse;
-import com.pawith.authmodule.application.dto.Provider;
-
-public interface OAuthFactory {
-    OAuthResponse login(Provider provider, String accessToken);
-}
