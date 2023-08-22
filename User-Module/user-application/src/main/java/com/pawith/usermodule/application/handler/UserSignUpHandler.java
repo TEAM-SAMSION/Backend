6349f736a8d8752b74edbd752eea22e214fe0035
tlarbals824 <simkm0824@naver.com>
2023-08-22 11:00:40 +0900

    PET-72 fix : EventHandler Component 설정 추가

--- User-Module/user-application/src/main/java/com/pawith/usermodule/application/handler/UserSignUpHandler.java
+++ User-Module/user-application/src/main/java/com/pawith/usermodule/application/handler/UserSignUpHandler.java
@@ -9,9 +9,11 @@ import com.pawith.usermodule.domain.service.UserSaveService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class UserSignUpHandler {
 
