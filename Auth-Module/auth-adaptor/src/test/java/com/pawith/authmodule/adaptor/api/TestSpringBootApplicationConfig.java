a3c9687936a157ee86312f6d549217f20da3b6b6
tlarbals824 <simkm0824@naver.com>
2023-08-27 21:49:43 +0900

    PET-80 test : OAuthController 테스트 추가 및 Auth-API.adoc 추가

---
+++ Auth-Module/auth-adaptor/src/test/java/com/pawith/authmodule/adaptor/api/TestSpringBootApplicationConfig.java
@@ -0,0 +1,7 @@
+package com.pawith.authmodule.adaptor.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootApplicationConfig {
+}
