dc8f7f46b0d380fe23f534cc07aaef4ebda6bd3b
tlarbals824 <simkm0824@naver.com>
2023-09-06 19:52:04 +0900

    PET-89 feat : ApplicationService 애노테이션 추가

---
+++ Common-Module/src/main/java/com/pawith/commonmodule/annotation/ApplicationService.java
@@ -0,0 +1,14 @@
+package com.pawith.commonmodule.annotation;
+
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Service
+public @interface ApplicationService {
+}
