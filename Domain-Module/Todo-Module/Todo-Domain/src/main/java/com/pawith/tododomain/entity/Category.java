fe5ccc32e10d4d362260b6f1e1a58c85e4e69c13
seeunjang <wisjang1@naver.com>
2023-09-08 17:28:23 +0900

    PET-96 feat : Category Entity 생성 및 TodoTeam과 연관관계 생성

--- Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/entity/Category.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/entity/Category.java
@@ -7,26 +7,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class TodoTeam extends BaseEntity {
-
+public class Category extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "team_id")
+    @Column(name = "category_id")
     private Long id;
 
-    private String teamCode;
-    private String teamName;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private CategoryStatus categoryStatus;
 
-    @OneToMany(mappedBy = "todoTeam", cascade = CascadeType.ALL)
-    private List<Pet> pets = new ArrayList<>();
-
-    @OneToMany(mappedBy = "todoTeam", cascade = CascadeType.ALL)
-    private List<Category> categories = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private TodoTeam todoTeam;
 }
