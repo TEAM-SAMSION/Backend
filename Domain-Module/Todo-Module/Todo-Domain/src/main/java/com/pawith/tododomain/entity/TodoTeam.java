96cfdfaa9bd70556962cf950d7e7f6d509b6116a
seeunjang <wisjang1@naver.com>
2023-09-08 17:18:43 +0900

    PET-96 feat : TodoTeam Entity 생성 및 Todo-Module에 Common-Module 의존성 추가

---
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/entity/TodoTeam.java
@@ -0,0 +1,24 @@
+package com.pawith.tododomain.entity;
+
+import com.pawith.commonmodule.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TodoTeam extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    private String teamCode;
+    private String teamName;
+}
