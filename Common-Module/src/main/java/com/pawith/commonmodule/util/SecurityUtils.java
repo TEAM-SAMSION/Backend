6fc9b3c7b99fd8017856997bc910145e3649a8e4
tlarbals824 <simkm0824@naver.com>
2023-09-08 12:06:20 +0900

    PET-93 refactor : SecurityContextHolder 패키지 경로 import로 수정

--- Common-Module/src/main/java/com/pawith/commonmodule/util/SecurityUtils.java
+++ Common-Module/src/main/java/com/pawith/commonmodule/util/SecurityUtils.java
@@ -2,12 +2,13 @@ package com.pawith.commonmodule.util;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SecurityUtils {
 
     public static String getAuthenticationPrincipal() {
-        final String email = (String) org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        final String email = (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return email;
     }
 }
