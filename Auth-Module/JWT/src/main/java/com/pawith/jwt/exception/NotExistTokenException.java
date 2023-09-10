25ab02edc524c3924792d84a4965ce2479f6dae2
tlarbals824 <simkm0824@naver.com>
2023-09-10 18:24:58 +0900

    PET-94 feat : Token 조회 예외 추가

--- Auth-Module/JWT/src/main/java/com/pawith/jwt/exception/NotExistTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.pawith.jwt.exception;
-
-import com.pawith.commonmodule.exception.Error;
-
-public class NotExistTokenException extends TokenException{
-    public NotExistTokenException(Error error) {
-        super(error);
-    }
-}
