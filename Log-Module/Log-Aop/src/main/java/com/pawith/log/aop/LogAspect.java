76fd319efbaf529a604d1bbe33dbeb5e0e0b0ab0
tlarbals824 <simkm0824@naver.com>
2023-09-12 13:03:23 +0900

    PET-98 refactor : logging서버로 로깅 정보 보내는 클래스 비활성화

--- Log-Module/Log-Aop/src/main/java/com/pawith/log/aop/LogAspect.java
+++ Log-Module/Log-Aop/src/main/java/com/pawith/log/aop/LogAspect.java
@@ -1,7 +1,7 @@
 package com.pawith.log.aop;
 
 import com.pawith.commonmodule.exception.BusinessException;
-import com.pawith.log.webclient.LogDataProcessor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -11,16 +11,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
+
 public class LogAspect {
 
     private final LogTrace logTrace;
-    private final LogDataProcessor logDataProcessor;
-
-
-    public LogAspect(LogTrace logTrace, LogDataProcessor logDataProcessor) {
-        this.logTrace = logTrace;
-        this.logDataProcessor = logDataProcessor;
-    }
+//    private final LogDataProcessor logDataProcessor;
 
     @Around("com.pawith.log.aop.Pointcuts.allService()")
     public Object serviceLog(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -39,18 +35,18 @@ public class LogAspect {
             traceStatus = logTrace.start(joinPoint.getSignature().getName());
             Object result = joinPoint.proceed();
             Integer executionTime = logTrace.end(traceStatus);
-            logDataProcessor.processLogData(traceStatus.getThreadId(), executionTime, traceStatus.getMethodName(), null);
+//            logDataProcessor.processLogData(traceStatus.getThreadId(), executionTime, traceStatus.getMethodName(), null);
             return result;
         } catch (BusinessException e) {
             if (traceStatus != null) {
                 logTrace.apiException(e, traceStatus);
-                logDataProcessor.processLogData(traceStatus.getThreadId(),0,traceStatus.getMethodName(), e.getError().getMessage());
+//                logDataProcessor.processLogData(traceStatus.getThreadId(),0,traceStatus.getMethodName(), e.getError().getMessage());
             }
             throw e;
         }catch (Exception e) {
             if (traceStatus != null) {
                 logTrace.exception(e, traceStatus);
-                logDataProcessor.processLogData(traceStatus.getThreadId(),0, traceStatus.getMethodName(), e.getMessage());
+//                logDataProcessor.processLogData(traceStatus.getThreadId(),0, traceStatus.getMethodName(), e.getMessage());
             }
             throw e;
         }
