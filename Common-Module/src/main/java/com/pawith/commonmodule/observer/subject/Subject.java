3171a90c0e7256c97f0820a34b035fe56eb13382
tlarbals824 <simkm0824@naver.com>
2023-09-15 12:27:33 +0900

    PET-103 refactor : 미사용 클래스 제거(Observer, LocalObjectRegistry)

--- Common-Module/src/main/java/com/pawith/commonmodule/observer/subject/Subject.java
+++
@@ -1,9 +0,0 @@
-package com.pawith.commonmodule.observer.subject;
-
-import com.pawith.commonmodule.observer.observer.Observer;
-
-public interface Subject<T,R> {
-    void registerObserver(Observer<? extends Status, ?> o);
-    void removeObserver(Observer<? extends Status, ?> o);
-    R notifyObservers(T object);
-}
