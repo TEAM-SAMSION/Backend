fef6811131df4fb6b0f31521bc1a55f7abc036e9
tlarbals824 <simkm0824@naver.com>
2023-09-15 12:28:06 +0900

    PET-103 refactor : 이미지 업로드 설정 파일 위치 변경 (Common->Image-Application)

--- Domain-Module/Image-Module/Image-Application/src/main/java/com/pawith/imageapplication/config/S3Config.java
+++ Domain-Module/Image-Module/Image-Application/src/main/java/com/pawith/imageapplication/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.pawith.commonmodule.config;
+package com.pawith.imageapplication.config;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
