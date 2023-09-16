0374d559c7aefa43012abfc9c18f1e41a824ef1f
tlarbals824 <simkm0824@naver.com>
2023-09-16 21:53:15 +0900

    PET-99 test : 사용자 이미지 업로드 API 테스트 및 명세 추가

--- Domain-Module/User-Module/User-Presentation/src/test/java/com/pawith/userpresentation/UserControllerTest.java
+++ Domain-Module/User-Module/User-Presentation/src/test/java/com/pawith/userpresentation/UserControllerTest.java
@@ -7,19 +7,22 @@ import com.pawith.usermodule.dto.request.UserNicknameChangeRequest;
 import com.pawith.usermodule.dto.response.UserInfoResponse;
 import com.pawith.usermodule.service.UserInfoGetUseCase;
 import com.pawith.usermodule.service.UserNicknameChangeUseCase;
+import com.pawith.usermodule.service.UserProfileImageUpdateUseCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -30,6 +33,8 @@ class UserControllerTest extends BaseRestDocsTest {
     private UserNicknameChangeUseCase userNicknameChangeUseCase;
     @MockBean
     private UserInfoGetUseCase userInfoGetUseCase;
+    @MockBean
+    private UserProfileImageUpdateUseCase userProfileImageUpdateUseCase;
 
     private static final String USER_REQUEST_URL = "/user";
     private static final String ACCESS_TOKEN = "Bearer accessToken";
@@ -79,6 +84,29 @@ class UserControllerTest extends BaseRestDocsTest {
                     fieldWithPath("nickname").description("유저 닉네임"),
                     fieldWithPath("email").description("유저 이메일"),
                     fieldWithPath("profileImageUrl").description("유저 프로필 이미지")
+                )
+            ));
+    }
+
+    @Test
+    @DisplayName("유저 프로필 이미지 업데이트 테스트")
+    void postUserProfileImage() throws Exception {
+        //given
+        final MockMultipartFile profileImage = new MockMultipartFile("profileImage", "profileImage".getBytes());
+        final MockHttpServletRequestBuilder request = multipart(USER_REQUEST_URL)
+            .file(profileImage)
+            .contentType("multipart/form-data")
+            .header(AUTHORIZATION_HEADER, ACCESS_TOKEN);
+        //when
+        ResultActions result = mvc.perform(request);
+        //then
+        result.andExpect(status().isOk())
+            .andDo(resultHandler.document(
+                requestHeaders(
+                    headerWithName(AUTHORIZATION_HEADER).description("access 토큰")
+                ),
+                requestParts(
+                    partWithName("profileImage").description("유저 프로필 이미지")
                 )
             ));
     }
