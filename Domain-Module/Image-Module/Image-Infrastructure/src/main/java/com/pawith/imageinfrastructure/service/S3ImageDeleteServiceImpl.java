ec6cfded7aff5110f844933eb789b45493249c3f
tlarbals824 <simkm0824@naver.com>
2023-09-18 15:35:05 +0900

    PET-108 feat : Image-Infrastructure 이미지 업로드 인터페이스 구현체 정의

--- Domain-Module/Image-Module/Image-Infrastructure/src/main/java/com/pawith/imageinfrastructure/service/S3ImageDeleteServiceImpl.java
+++ Domain-Module/Image-Module/Image-Infrastructure/src/main/java/com/pawith/imageinfrastructure/service/S3ImageDeleteServiceImpl.java
@@ -4,8 +4,8 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.pawith.commonmodule.annotation.ApplicationService;
 import com.pawith.commonmodule.exception.Error;
-import com.pawith.imageinfrastructure.exception.FileDeleteException;
-import com.pawith.imageapplication.service.ImageDeleteUseCase;
+import com.pawith.imageapplication.exception.FileDeleteException;
+import com.pawith.imagedomain.service.ImageDeleteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,7 +16,7 @@ import java.util.function.Function;
 @ApplicationService
 @RequiredArgsConstructor
 @Slf4j
-public class S3ImageDeleteUseCaseImpl<T> implements ImageDeleteUseCase<T> {
+public class S3ImageDeleteServiceImpl<T> implements ImageDeleteService<T> {
     private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.s3.bucket}")
