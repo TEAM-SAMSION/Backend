a0ec61e70c4c2e28328394e310a3828a8ee87193
seeunjang <wisjang1@naver.com>
2023-09-19 00:16:35 +0900

    PET-107 test : 가입한 팀 이름 조회 API 명세 추가

--- Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/TodoHomeControllerTest.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/TodoHomeControllerTest.java
@@ -4,7 +4,9 @@ import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 import com.pawith.commonmodule.BaseRestDocsTest;
 import com.pawith.todoapplication.dto.response.TodoProgressResponse;
+import com.pawith.todoapplication.dto.response.TodoTeamNameSimpleResponse;
 import com.pawith.todoapplication.service.TodoRateGetUseCase;
+import com.pawith.todoapplication.service.TodoTeamGetUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
@@ -30,6 +34,9 @@ public class TodoHomeControllerTest extends BaseRestDocsTest {
 
     @MockBean
     private TodoRateGetUseCase todoRateGetUseCase;
+
+    @MockBean
+    private TodoTeamGetUseCase todoTeamGetUseCase;
 
     private static final String TODO_HOME_REQUEST_URL = "/todo/home";
 
@@ -55,6 +62,29 @@ public class TodoHomeControllerTest extends BaseRestDocsTest {
                         ),
                         responseFields(
                                 fieldWithPath("progress").description("달성률")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("가입한 팀 조회 API 테스트")
+    void getRecentTodoTeamName() throws Exception{
+        //given
+        final List<TodoTeamNameSimpleResponse> todoTeamNameSimpleResponses = getFixtureMonkey().giveMe(TodoTeamNameSimpleResponse.class,5);
+        given(todoTeamGetUseCase.getTodoTeamName()).willReturn(todoTeamNameSimpleResponses);
+        MockHttpServletRequestBuilder request = get(TODO_HOME_REQUEST_URL)
+                .header("Authorization", "Bearer accessToken");
+        //when
+        ResultActions result = mvc.perform(request);
+        //then
+        result.andExpect(status().isOk())
+                .andDo(resultHandler.document(
+                        requestHeaders(
+                                headerWithName("Authorization").description("access 토큰")
+                        ),
+                        responseFields(
+                                fieldWithPath("[].teamId").description("TodoTeam의 Id"),
+                                fieldWithPath("[].teamName").description("TodoTeam의 이름")
                         )
                 ));
     }
