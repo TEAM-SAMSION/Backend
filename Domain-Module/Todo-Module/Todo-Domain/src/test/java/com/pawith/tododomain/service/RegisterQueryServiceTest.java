cee5fad2052f38ddfb28c8f74c134c36aea8e6e3
tlarbals824 <simkm0824@naver.com>
2023-09-20 00:04:54 +0900

    PET-111 test : Register 엔티티 관련 서비스 테스트 코드 추가

---
+++ Domain-Module/Todo-Module/Todo-Domain/src/test/java/com/pawith/tododomain/service/RegisterQueryServiceTest.java
@@ -0,0 +1,92 @@
+package com.pawith.tododomain.service;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.pawith.commonmodule.UnitTestConfig;
+import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
+import com.pawith.tododomain.entity.Register;
+import com.pawith.tododomain.exception.NotRegisterUserException;
+import com.pawith.tododomain.repository.RegisterRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+
+@UnitTestConfig
+@DisplayName("RegisterQueryService 테스트")
+class RegisterQueryServiceTest {
+
+    @Mock
+    private RegisterRepository registerRepository;
+
+    private RegisterQueryService registerQueryService;
+
+    @BeforeEach
+    void init(){
+        registerQueryService = new RegisterQueryService(registerRepository);
+    }
+
+    @Test
+    @DisplayName("todoTeamId와 userId로 Register 엔티티를 조회한다.")
+    void findByTodoTeamIdAndUserId() {
+        //given
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Register mockRegister = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMeOne(Register.class);
+        given(registerRepository.findByTodoTeamIdAndUserId(todoTeamId, userId)).willReturn(Optional.of(mockRegister));
+        //when
+        Register result = registerQueryService.findRegisterByTodoTeamIdAndUserId(todoTeamId, userId);
+        //then
+        Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(mockRegister);
+    }
+
+    @Test
+    @DisplayName("todoTeamId와 userId로 Register 엔티티를 조회하지 못하면 NotRegisterUserException이 발생한다.")
+    void findByTodoTeamIdAndUserIdFail() {
+        //given
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        given(registerRepository.findByTodoTeamIdAndUserId(todoTeamId, userId)).willReturn(Optional.empty());
+        //when
+        //then
+        Assertions.assertThatCode(() -> registerQueryService.findRegisterByTodoTeamIdAndUserId(todoTeamId, userId))
+            .isInstanceOf(NotRegisterUserException.class);
+    }
+
+    @Test
+    @DisplayName("userId로 모든 Register 엔티티를 조회한다.")
+    void findAllByUserId() {
+        //given
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        final List<Register> mockRegister = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMe(Register.class, 10);
+        given(registerRepository.findAllByUserId(userId)).willReturn(mockRegister);
+        //when
+        List<Register> result = registerQueryService.findRegisterByUserId(userId);
+        //then
+        Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(mockRegister);
+    }
+
+    @Test
+    @DisplayName("userId와 Pageable로 Register 엔티티를 조회한다.")
+    void findRegisterSliceByUserId() {
+        //given
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        final PageRequest pageRequest = PageRequest.of(0, 10);
+        final List<Register> mockRegister = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMe(Register.class, 10);
+        SliceImpl<Register> mockSlice = new SliceImpl<>(mockRegister, pageRequest, true);
+        given(registerRepository.findAllByUserId(userId,pageRequest)).willReturn(mockSlice);
+        //when
+        Slice<Register> result = registerQueryService.findRegisterSliceByUserId(userId,pageRequest);
+        //then
+        Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(mockSlice);
+    }
+
+}
---
+++ Domain-Module/Todo-Module/Todo-Domain/src/test/java/com/pawith/tododomain/service/RegisterSaveServiceTest.java
@@ -0,0 +1,71 @@
+package com.pawith.tododomain.service;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.pawith.commonmodule.UnitTestConfig;
+import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
+import com.pawith.tododomain.entity.TodoTeam;
+import com.pawith.tododomain.exception.AlreadyRegisterTodoTeamException;
+import com.pawith.tododomain.repository.RegisterRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@UnitTestConfig
+@DisplayName("RegisterSaveService 테스트")
+class RegisterSaveServiceTest {
+
+    @Mock
+    private RegisterRepository registerRepository;
+
+    private RegisterSaveService registerSaveService;
+
+    @BeforeEach
+    void init() {
+        registerSaveService = new RegisterSaveService(registerRepository);
+    }
+
+    @Test
+    @DisplayName("todoTeam 엔티티와 userId를 입력받아 member 권한 Register 엔티티를 저장한다.")
+    void saveRegisterAboutMember() {
+        //given
+        final TodoTeam todoTeam = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMeOne(TodoTeam.class);
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        given(registerRepository.existsByTodoTeamIdAndUserId(todoTeam.getId(), userId)).willReturn(false);
+        //when
+        registerSaveService.saveRegisterAboutMember(todoTeam, userId);
+        //then
+        then(registerRepository).should().save(any());
+    }
+
+    @Test
+    @DisplayName("todoTeam 엔티티와 userId를 입력받아 president 권한 Register 엔티티를 저장한다.")
+    void saveRegisterAboutPresident() {
+        //given
+        final TodoTeam todoTeam = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMeOne(TodoTeam.class);
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        given(registerRepository.existsByTodoTeamIdAndUserId(todoTeam.getId(), userId)).willReturn(false);
+        //when
+        registerSaveService.saveRegisterAboutPresident(todoTeam, userId);
+        //then
+        then(registerRepository).should().save(any());
+    }
+
+    @Test
+    @DisplayName("todoTeam 엔티티와 userId를 입력받아 이미 등록된 회원이면 AlreadyRegisterTodoTeamException이 발생한다.")
+    void saveRegisterAboutMemberFail() {
+        //given
+        final TodoTeam todoTeam = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMeOne(TodoTeam.class);
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        given(registerRepository.existsByTodoTeamIdAndUserId(todoTeam.getId(), userId)).willReturn(true);
+        //when
+        //then
+        Assertions.assertThatCode(() -> registerSaveService.saveRegisterAboutMember(todoTeam, userId))
+                .isInstanceOf(AlreadyRegisterTodoTeamException.class);
+    }
+}
