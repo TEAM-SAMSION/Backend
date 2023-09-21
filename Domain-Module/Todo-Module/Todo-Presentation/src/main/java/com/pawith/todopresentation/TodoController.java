e5050d9b2547f5c37930003d75d0d8328a0ed2ea
seeunjang <wisjang1@naver.com>
2023-09-21 13:38:29 +0900

    PET-115 refactor : TodoTeamController 생성, TodoHomeController 제거 및 메서드 이동

--- Domain-Module/Todo-Module/Todo-Presentation/src/main/java/com/pawith/todopresentation/TodoController.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/main/java/com/pawith/todopresentation/TodoController.java
@@ -3,31 +3,27 @@ package com.pawith.todopresentation;
 import com.pawith.commonmodule.slice.SliceResponse;
 import com.pawith.todoapplication.dto.response.TodoHomeResponse;
 import com.pawith.todoapplication.dto.response.TodoProgressResponse;
-import com.pawith.todoapplication.dto.response.TodoTeamNameSimpleResponse;
 import com.pawith.todoapplication.service.TodoGetUseCase;
 import com.pawith.todoapplication.service.TodoRateGetUseCase;
-import com.pawith.todoapplication.service.TodoTeamGetUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import org.springframework.data.domain.Pageable;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/todo/home")
-public class TodoHomeController {
-
-    private final TodoRateGetUseCase todoRateGetUseCase;
-    private final TodoTeamGetUseCase todoTeamGetUseCase;
+@RequestMapping("/todo")
+public class TodoController {
     private final TodoGetUseCase todoGetUseCase;
+    private final TodoRateGetUseCase todoRateGetUseCase;
 
-    @GetMapping
-    public List<TodoTeamNameSimpleResponse> getTodoTeamName() {return todoTeamGetUseCase.getTodoTeamName();}
     @GetMapping("/progress/{teamId}")
     public TodoProgressResponse getTodoProgress(@PathVariable Long teamId) { return todoRateGetUseCase.getTodoProgress(teamId);}
+
 
     @GetMapping("/list/{teamId}")
     public SliceResponse<TodoHomeResponse> getTodos(@PathVariable Long teamId, Pageable pageable) { return todoGetUseCase.getTodos(teamId, pageable);}
