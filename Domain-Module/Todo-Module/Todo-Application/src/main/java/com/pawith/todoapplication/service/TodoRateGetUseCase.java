abfb4314e5665278ff6f991e55ab5b7c663a19b3
tlarbals824 <simkm0824@naver.com>
2023-09-22 11:13:12 +0900

    PET-117 refactor : Todo 완료 조회 usecase 리팩터링

--- Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/service/TodoRateGetUseCase.java
+++ Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/service/TodoRateGetUseCase.java
@@ -2,19 +2,11 @@ package com.pawith.todoapplication.service;
 
 import com.pawith.commonmodule.annotation.ApplicationService;
 import com.pawith.todoapplication.dto.response.TodoProgressResponse;
-import com.pawith.tododomain.entity.Assign;
-import com.pawith.tododomain.entity.Register;
-import com.pawith.tododomain.entity.Todo;
-import com.pawith.tododomain.service.*;
+import com.pawith.tododomain.service.TodoQueryService;
 import com.pawith.userdomain.entity.User;
 import com.pawith.userdomain.utils.UserUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @ApplicationService
 @RequiredArgsConstructor
@@ -23,23 +15,10 @@ public class TodoRateGetUseCase {
 
     private final UserUtils userUtils;
     private final TodoQueryService todoQueryService;
-    private final RegisterQueryService registerQueryService;
-    private final AssignQueryService assignQueryService;
 
-    public TodoProgressResponse getTodoProgress(Long todoTeamId) {
-        User user = userUtils.getAccessUser();
-        Register register = registerQueryService.findRegisterByTodoTeamIdAndUserId(todoTeamId, user.getId());
-        List<Assign> assignList = assignQueryService.findTodayAssignByRegisterId(register.getId());
-        List<Todo> todoList = assignList.stream()
-                .map(assign -> todoQueryService.findTodoByTodoId(assign.getTodo().getId()))
-                .collect(Collectors.toList());
-        Integer totalTodoCount = todoList.size();
-
-        Long doneTodoCount = todoList.stream()
-                .filter(todo -> todo.getTodoStatus().equals("COMPLETE"))
-                .count();
-
-        int result =  (int)((double)doneTodoCount/totalTodoCount*100);
-        return new TodoProgressResponse(result);
+    public TodoProgressResponse getTodoProgress(final Long todoTeamId) {
+        final User user = userUtils.getAccessUser();
+        final Integer todoCompleteRate = todoQueryService.findTodoCompleteRate(user.getId(), todoTeamId);
+        return new TodoProgressResponse(todoCompleteRate);
     }
 }
