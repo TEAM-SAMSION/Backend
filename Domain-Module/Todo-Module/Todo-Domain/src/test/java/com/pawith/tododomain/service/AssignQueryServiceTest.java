b46fb8b2e020ff079cfa03b47d6b642421d0d359
tlarbals824 <simkm0824@naver.com>
2023-09-22 11:15:16 +0900

    PET-117 test : AssignQueryServiceTest 수정

--- Domain-Module/Todo-Module/Todo-Domain/src/test/java/com/pawith/tododomain/service/AssignQueryServiceTest.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/test/java/com/pawith/tododomain/service/AssignQueryServiceTest.java
@@ -10,8 +10,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
@@ -30,19 +35,20 @@ class AssignQueryServiceTest {
         assignQueryService = new AssignQueryService(assignRepository);
     }
 
-//    @Test
-//    @DisplayName("registerId와 날짜를 받아 해당 날짜의 할당 목록을 조회한다.")
-//    void findAssignByRegisterIdAndCreatedAtBetween() {
-//        //given
-//        final Long mockRegisterId = FixtureMonkey.create().giveMeOne(Long.class);
-//        final LocalDateTime mockStartOfDay = FixtureMonkey.create().giveMeOne(LocalDateTime.class);
-//        final LocalDateTime mockEndOfDay = FixtureMonkey.create().giveMeOne(LocalDateTime.class);
-//        final List<Assign> mockAssign = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMe(Assign.class,10);
-//        given(assignRepository.findAllByRegisterIdAndCreatedAtBetween(mockRegisterId, mockStartOfDay, mockEndOfDay)).willReturn(mockAssign);
-//        //when
-//        List<Assign> result = assignQueryService.findAssignByRegisterIdAndCreatedAtBetween(mockRegisterId, mockStartOfDay, mockEndOfDay);
-//        //then
-//       Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(mockAssign);
-//    }
+    @Test
+    @DisplayName("registerId와 날짜를 받아 해당 날짜의 할당 목록을 조회한다.")
+    void findAssignByRegisterIdAndCreatedAtBetween() {
+        //given
+        final Pageable pageable = PageRequest.of(0,10);
+        final Long mockRegisterId = FixtureMonkey.create().giveMeOne(Long.class);
+        final List<Assign> mockAssign = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMe(Assign.class,10);
+        final Slice<Assign> mockSlice = new SliceImpl<>(mockAssign, pageable, true);
+        final LocalDate now = LocalDate.now();
+        given(assignRepository.findAllByRegisterIdAndCreatedAtBetween(mockRegisterId, now.atStartOfDay(),now.atTime(LocalTime.MAX), pageable)).willReturn(mockSlice);
+        //when
+        Slice<Assign> result = assignQueryService.findTodayAssignSliceByRegisterId(mockRegisterId, pageable);
+        //then
+       Assertions.assertThat(result.getContent().size()).isEqualTo(pageable.getPageSize());
+    }
 
 }
