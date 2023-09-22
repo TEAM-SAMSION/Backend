16b9aa16210ca93da4e27bbeb157d5fbb18d5520
tlarbals824 <simkm0824@naver.com>
2023-09-22 11:12:34 +0900

    PET-117 feat : Todo 완료율 조회하는 TodoQueryService 메소드 추가

--- Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/TodoRepository.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.pawith.tododomain.repository;
 
 import com.pawith.tododomain.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 
@@ -9,4 +11,21 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     Long countByCategoryIdAndCreatedAt(Long categoryId, LocalDateTime createdAt);
 
     Long countByCategoryIdAndCreatedAtAndTodoStatus(Long categoryId, LocalDateTime createdAt, String status);
+
+    @Query(value = "select count(t) " +
+        "from Todo t " +
+        "join Register r on r.userId=:userId and r.todoTeam.id=:todoTeamId " +
+        "join Assign  a on a.register.id=r.id " +
+        "where a.todo.id=t.id and date(a.createdAt) = date(now())"
+    )
+    Long countTodayTodo(@Param("userId") Long userId, @Param("todoTeamId") Long todoTeamId);
+
+    @Query(value = "select count(t) " +
+        "from Todo t " +
+        "join Register r on r.userId=:userId and r.todoTeam.id=:todoTeamId " +
+        "join Assign  a on a.register.id=r.id " +
+        "where a.todo.id=t.id and date(a.createdAt) = date(now()) and t.todoStatus='COMPLETE'"
+    )
+    Long countTodayCompleteTodo(@Param("userId") Long userId, @Param("todoTeamId") Long todoTeamId);
+
 }
