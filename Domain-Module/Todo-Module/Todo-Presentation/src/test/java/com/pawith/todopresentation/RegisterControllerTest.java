3ec5c8bf827393e753781f6d2fd102a6a67dc750
tlarbals824 <simkm0824@naver.com>
2023-09-24 15:58:36 +0900

    PET-121 test : TodoTeam 등록된 사용자 조회 및 Todo 생성 API 테스트 및 명세 추가

--- Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/RegisterControllerTest.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/RegisterControllerTest.java
@@ -2,6 +2,10 @@ package com.pawith.todopresentation;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.pawith.commonmodule.BaseRestDocsTest;
+import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
+import com.pawith.todoapplication.dto.response.RegisterListResponse;
+import com.pawith.todoapplication.dto.response.RegisterSimpleInfoResponse;
+import com.pawith.todoapplication.service.RegistersGetUseCase;
 import com.pawith.todoapplication.service.TodoTeamRegisterUseCase;
 import com.pawith.todoapplication.service.UnregisterUseCase;
 import org.junit.jupiter.api.DisplayName;
@@ -12,11 +16,15 @@ import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import java.util.List;
 import java.util.UUID;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +36,8 @@ class RegisterControllerTest extends BaseRestDocsTest {
     private UnregisterUseCase unregisterUseCase;
     @MockBean
     private TodoTeamRegisterUseCase todoTeamRegisterUseCase;
+    @MockBean
+    private RegistersGetUseCase registersGetUseCase;
 
     private static final String REGISTER_REQUEST_URL = "/register";
 
@@ -72,5 +82,34 @@ class RegisterControllerTest extends BaseRestDocsTest {
                     parameterWithName("todoTeamCode").description("TodoTeam의 코드")
                 )
             ));
+    }
+
+    @Test
+    @DisplayName("TodoTeam의 등록된 Register들을 조회하는 테스트")
+    void getRegisters() throws Exception {
+        //given
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final List<RegisterSimpleInfoResponse> registerSimpleInfoResponses = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMe(RegisterSimpleInfoResponse.class, 10);
+        final RegisterListResponse registerListResponse = new RegisterListResponse(registerSimpleInfoResponses);
+        given(registersGetUseCase.getRegisters(todoTeamId)).willReturn(registerListResponse);
+        MockHttpServletRequestBuilder request = get(REGISTER_REQUEST_URL + "/list?teamId={teamId}", todoTeamId)
+            .header("Authorization", "Bearer accessToken");
+        //when
+        ResultActions result = mvc.perform(request);
+        //then
+        result.andExpect(status().isOk())
+            .andDo(resultHandler.document(
+                requestHeaders(
+                    headerWithName("Authorization").description("access 토큰")
+                ),
+                requestParameters(
+                    parameterWithName("teamId").description("조회하는 TodoTeamId")
+                ),
+                responseFields(
+                    fieldWithPath("registers[].registerId").description("TodoTeam에 등록된 사용자 registerId"),
+                    fieldWithPath("registers[].registerName").description("TodoTeam에 등록된 사용자 이름")
+                )
+            ));
+
     }
 }
