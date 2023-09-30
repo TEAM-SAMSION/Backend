ed999f40543b18a7cb785360f76582da1298ecd3
tlarbals824 <simkm0824@naver.com>
2023-09-30 21:47:11 +0900

    PET-127 test : 로그아웃 API 테스트 및 명세 추가

--- Domain-Module/Auth-Module/Auth-Presentation/src/test/java/com/pawith/authpresentation/OAuthControllerTest.java
+++ Domain-Module/Auth-Module/Auth-Presentation/src/test/java/com/pawith/authpresentation/OAuthControllerTest.java
@@ -1,12 +1,13 @@
 package com.pawith.authpresentation;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
 import com.pawith.authapplication.dto.OAuthResponse;
+import com.pawith.authapplication.service.LogoutUseCase;
 import com.pawith.authapplication.service.OAuthUseCase;
 import com.pawith.authpresentation.common.FilterConfig;
 import com.pawith.commonmodule.BaseRestDocsTest;
 import com.pawith.commonmodule.enums.Provider;
+import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,6 +17,9 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -32,7 +36,9 @@ class OAuthControllerTest extends BaseRestDocsTest {
     private static final String OAUTH_REQUEST_ACCESS_TOKEN_PARAM_NAME = "accessToken";
 
     @MockBean
-    OAuthUseCase oAuthUseCase;
+    private OAuthUseCase oAuthUseCase;
+    @MockBean
+    private LogoutUseCase logoutUseCase;
 
     @Test
     @DisplayName("OAuth 로그인")
@@ -40,7 +46,7 @@ class OAuthControllerTest extends BaseRestDocsTest {
         //given
         final Provider testProvider = FixtureMonkey.create().giveMeOne(Provider.class);
         final String OAUTH_ACCESS_TOKEN = FixtureMonkey.create().giveMeOne(String.class);
-        final OAuthResponse testOAuthResponse = getFixtureMonkey().giveMeOne(OAuthResponse.class);
+        final OAuthResponse testOAuthResponse = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(OAuthResponse.class);
         MockHttpServletRequestBuilder request = get(OAUTH_URL, testProvider)
             .param(OAUTH_REQUEST_ACCESS_TOKEN_PARAM_NAME, OAUTH_ACCESS_TOKEN);
         given(oAuthUseCase.oAuthLogin(testProvider, OAUTH_ACCESS_TOKEN)).willReturn(testOAuthResponse);
@@ -64,9 +70,23 @@ class OAuthControllerTest extends BaseRestDocsTest {
 
     }
 
-    private FixtureMonkey getFixtureMonkey() {
-        return FixtureMonkey.builder()
-            .objectIntrospector(BuilderArbitraryIntrospector.INSTANCE)
-            .build();
+    @Test
+    @DisplayName("로그아웃")
+    void logout() throws Exception {
+        //given
+        MockHttpServletRequestBuilder request = delete("/logout")
+            .header("Authorization","Bearer accessToken")
+            .header("RefreshToken","Bearer refreshToken");
+        //when
+        ResultActions result = mvc.perform(request);
+        //then
+        result.andExpect(status().isOk())
+            .andDo(print())
+            .andDo(resultHandler.document(
+                requestHeaders(
+                    headerWithName("Authorization").description("access 토큰"),
+                    headerWithName("RefreshToken").description("refresh 토큰")
+                )
+            ));
     }
 }
