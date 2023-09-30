5eac2f85a3710aa3c6fe1b1be81364725417091b
tlarbals824 <simkm0824@naver.com>
2023-09-30 21:52:17 +0900

    PET-127 feat : 토큰 헤더 파싱 전용 유틸 클래스 추가

---
+++ Domain-Module/Auth-Module/Auth-Application/src/main/java/com/pawith/authapplication/utils/TokenExtractUtils.java
@@ -0,0 +1,28 @@
+package com.pawith.authapplication.utils;
+
+import com.pawith.authapplication.consts.AuthConsts;
+import com.pawith.authapplication.exception.InvalidAuthorizationTypeException;
+import com.pawith.commonmodule.exception.Error;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TokenExtractUtils {
+
+    public static String extractToken(final String tokenHeader) {
+        if(!StringUtils.hasText(tokenHeader)){
+            throw new InvalidAuthorizationTypeException(Error.EMPTY_AUTHORIZATION_HEADER);
+        }
+
+        final String[] splitToken = tokenHeader.split(" ");
+        final String authorizationType = splitToken[0];
+        final String accessToken = splitToken[1];
+        if(!Objects.equals(authorizationType, AuthConsts.AUTHENTICATION_TYPE)){
+            throw new InvalidAuthorizationTypeException(Error.INVALID_AUTHORIZATION_TYPE);
+        }
+        return accessToken;
+    }
+}
