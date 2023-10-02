3bc90a1bf4ff9b5644c936342783928676a1f403
seeunjang <wisjang1@naver.com>
2023-10-02 12:06:42 +0900

    PET-128 test : Todo 서비스 코드 작성

--- Domain-Module/Todo-Module/Todo-Domain/src/test/java/com/pawith/tododomain/service/TodoQueryServiceTest.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/test/java/com/pawith/tododomain/service/TodoQueryServiceTest.java
@@ -10,7 +10,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.BDDMockito.given;
@@ -54,5 +59,90 @@ class TodoQueryServiceTest {
         Assertions.assertThatCode(() -> todoQueryService.findTodoByTodoId(todoId))
             .isInstanceOf(RuntimeException.class);
     }
+
+    @Test
+    @DisplayName("Todo의 완료율을 조회한다.")
+    void findTodoCompleteRate() {
+        //given
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final LocalDate now = LocalDate.now();
+        final Long mockCountTodayTodo = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long mockCountCompleteTodayTodo = FixtureMonkey.create().giveMeOne(Long.class);
+        given(todoRepository.countTodoByDate(userId, todoTeamId, now)).willReturn(mockCountTodayTodo);
+        given(todoRepository.countCompleteTodoByDate(userId, todoTeamId, now)).willReturn(mockCountCompleteTodayTodo);
+        //when
+        Integer result = todoQueryService.findTodoCompleteRate(userId, todoTeamId);
+        //then
+        Assertions.assertThat(result).isEqualTo((int)(mockCountCompleteTodayTodo / (double) mockCountTodayTodo * 100));
+    }
+
+    @Test
+    @DisplayName("오늘 할당받은 Todo를 조회한다.")
+    void findTodayTodoSlice() {
+        //given
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final PageRequest pageRequest = PageRequest.of(0, 10);
+        final LocalDate now = LocalDate.now();
+        final List<Todo> mockTodoList = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMe(Todo.class, 10);
+        SliceImpl<Todo> mockSlice = new SliceImpl<>(mockTodoList, pageRequest, true);
+        given(todoRepository.findTodoByDate(userId, todoTeamId, now, pageRequest)).willReturn(mockSlice);
+        //when
+        Slice<Todo> result = todoQueryService.findTodayTodoSlice(userId, todoTeamId, pageRequest);
+        //then
+        Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(mockSlice);
+    }
+
+    @Test
+    @DisplayName("categoryId와 moveDate로 Todo를 조회한다.")
+    void findTodoListByCategoryIdAndscheduledDate() {
+        //given
+        final Long categoryId = FixtureMonkey.create().giveMeOne(Long.class);
+        final LocalDate moveDate = FixtureMonkey.create().giveMeOne(LocalDate.class);
+        final List<Todo> mockTodoList = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMe(Todo.class, 10);
+        given(todoRepository.findTodoListByCategoryIdAndscheduledDate(categoryId, moveDate)).willReturn(mockTodoList);
+        //when
+        List<Todo> result = todoQueryService.findTodoListByCategoryIdAndscheduledDate(categoryId, moveDate);
+        //then
+        Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(mockTodoList);
+    }
+
+    @Test
+    @DisplayName("이번 주의 Todo의 완료율을 조회한다.")
+    void findThisWeekTodoCompleteRate() {
+        //given
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final LocalDate now = LocalDate.now();
+        final LocalDate firstDayOfWeek = now.with(java.time.DayOfWeek.SUNDAY);
+        final Long mockCountWeekTodo = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long mockCountCompleteWeekTodo = FixtureMonkey.create().giveMeOne(Long.class);
+        given(todoRepository.countTodoByBetweenDate(userId, todoTeamId, now, firstDayOfWeek)).willReturn(mockCountWeekTodo);
+        given(todoRepository.countCompleteTodoByBetweenDate(userId, todoTeamId, now, firstDayOfWeek)).willReturn(mockCountCompleteWeekTodo);
+        //when
+        Integer result = todoQueryService.findThisWeekTodoCompleteRate(userId, todoTeamId);
+        //then
+        Assertions.assertThat(result).isEqualTo((int)(mockCountCompleteWeekTodo / (double) mockCountWeekTodo * 100));
+    }
+
+    @Test
+    @DisplayName("지난 주의 Todo의 완료율을 조회한다.")
+    void findLastWeekTodoCompleteRate() {
+        //given
+        final Long userId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final LocalDate now = LocalDate.now();
+        final LocalDate firstDayOfLastWeek = now.with(java.time.DayOfWeek.SUNDAY).minusWeeks(1);
+        final Long mockCountWeekTodo = FixtureMonkey.create().giveMeOne(Long.class);
+        final Long mockCountCompleteWeekTodo = FixtureMonkey.create().giveMeOne(Long.class);
+        given(todoRepository.countTodoByBetweenDate(userId, todoTeamId, now, firstDayOfLastWeek)).willReturn(mockCountWeekTodo);
+        given(todoRepository.countCompleteTodoByBetweenDate(userId, todoTeamId, now, firstDayOfLastWeek)).willReturn(mockCountCompleteWeekTodo);
+        //when
+        Integer result = todoQueryService.findLastWeekTodoCompleteRate(userId, todoTeamId);
+        //then
+        Assertions.assertThat(result).isEqualTo((int)(mockCountCompleteWeekTodo / (double) mockCountWeekTodo * 100));
+    }
+
 
 }
