a3dcc809f338dc1856cb41451bd046f328166585
seeunjang <wisjang1@naver.com>
2023-10-02 12:06:07 +0900

    PET-128 test : 주차 달성률 비교 UseCase 테스트 코드 작성

--- Domain-Module/Todo-Module/Todo-Application/src/test/java/com/pawith/todoapplication/service/TodoRateGetUseCaseTest.java
+++ Domain-Module/Todo-Module/Todo-Application/src/test/java/com/pawith/todoapplication/service/TodoRateGetUseCaseTest.java
@@ -4,6 +4,7 @@ import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.pawith.commonmodule.UnitTestConfig;
 import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
 import com.pawith.todoapplication.dto.response.TodoProgressResponse;
+import com.pawith.todoapplication.dto.response.TodoRateCompareResponse;
 import com.pawith.tododomain.service.TodoQueryService;
 import com.pawith.userdomain.entity.User;
 import com.pawith.userdomain.utils.UserUtils;
@@ -46,6 +47,25 @@ class TodoRateGetUseCaseTest {
         // then
         Assertions.assertThat(todoProgress).isNotNull();
         Assertions.assertThat(todoProgress.getProgress()).isEqualTo(todoCompleteRate);
+    }
+
+    @Test
+    @DisplayName("이번주 할당된 TODO 완료율과 지난주 할당된 TODO 완료율 비교 테스트")
+    void getWeekProgressCompare() {
+        // given
+        final User mockUser = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey()
+            .giveMeOne(User.class);
+        final Long todoTeamId = FixtureMonkey.create().giveMeOne(Long.class);
+        final Integer thisWeekTodoCompleteRate = (int) (Math.random() * 50) + 50;
+        final Integer lastWeekTodoCompleteRate = (int) (Math.random() * 50);
+        given(userUtils.getAccessUser()).willReturn(mockUser);
+        given(todoQueryService.findThisWeekTodoCompleteRate(mockUser.getId(), todoTeamId)).willReturn(thisWeekTodoCompleteRate);
+        given(todoQueryService.findLastWeekTodoCompleteRate(mockUser.getId(), todoTeamId)).willReturn(lastWeekTodoCompleteRate);
+        // when
+        TodoRateCompareResponse todoRateCompareResponse = todoRateGetUseCase.getWeekProgressCompare(todoTeamId);
+        // then
+        Assertions.assertThat(todoRateCompareResponse).isNotNull();
+        Assertions.assertThat(todoRateCompareResponse.getCompareWithLastWeek()).isEqualTo(TodoRateCompareResponse.ComparisonResult.HIGHER);
     }
 
 }
