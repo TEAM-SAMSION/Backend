3e2085b4acb00390ec8122a520ccbd6bae0e499c
tlarbals824 <simkm0824@naver.com>
2023-10-12 21:37:10 +0900

    PET-124 feat : CategoyId와 날짜를 통해 해당 Assign 리스트 반환 메소드 추가

--- Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/AssignRepository.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/AssignRepository.java
@@ -2,8 +2,18 @@ package com.pawith.tododomain.repository;
 
 import com.pawith.tododomain.entity.Assign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-
+import java.time.LocalDate;
+import java.util.List;
 
 public interface AssignRepository extends JpaRepository<Assign, Long> {
+
+    @Query("select a " +
+        "from Assign a " +
+        "join Category c on c.id=:categoryId "+
+        "join fetch a.register " +
+        "join fetch a.todo t " +
+        "where c.id=t.category.id and t.scheduledDate=:scheduledDate")
+    List<Assign> findAllByCategoryIdAndScheduledDate(Long categoryId, LocalDate scheduledDate);
 }
--- Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/service/AssignQueryService.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/service/AssignQueryService.java
@@ -1,12 +1,23 @@
 package com.pawith.tododomain.service;
 
 import com.pawith.commonmodule.annotation.DomainService;
+import com.pawith.tododomain.entity.Assign;
+import com.pawith.tododomain.repository.AssignRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @DomainService
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AssignQueryService {
+
+    private final AssignRepository assignRepository;
+
+    public List<Assign> findAllAssignByCategoryIdAndScheduledDate(Long categoryId, LocalDate scheduledDate) {
+        return assignRepository.findAllByCategoryIdAndScheduledDate(categoryId, scheduledDate);
+    }
 
 }
