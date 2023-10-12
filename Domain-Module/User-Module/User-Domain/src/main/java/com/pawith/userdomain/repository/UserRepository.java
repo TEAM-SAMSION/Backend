7287abc3b1e6c0709dce95407cfaad19dc48185e
tlarbals824 <simkm0824@naver.com>
2023-10-12 21:32:31 +0900

    PET-124 feat : User Ids 반환 메소드를 통한 key : userId , value : User 맵 반환 메소드 추가

--- Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/repository/UserRepository.java
+++ Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.pawith.userdomain.repository;
 
 import com.pawith.userdomain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    @Query("select u from User u where u.id in :ids")
+    List<User> findAllByIds(List<Long> ids);
 }
