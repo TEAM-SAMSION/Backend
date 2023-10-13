86f6d89b05291e7c50c01149078ff300d768beb0
tlarbals824 <simkm0824@naver.com>
2023-10-13 18:49:08 +0900

    PET-111 test : OAuthHandler 구현체(Google, Naver) 테스트 코드 작성

---
+++ Domain-Module/Auth-Module/Auth-Application/src/test/java/com/pawith/authapplication/service/command/handler/impl/NaverOAuthHandlerTest.java
@@ -0,0 +1,62 @@
+package com.pawith.authapplication.service.command.handler.impl;
+
+import com.pawith.authapplication.dto.OAuthRequest;
+import com.pawith.authapplication.dto.OAuthUserInfo;
+import com.pawith.authapplication.service.command.feign.NaverOAuthFeignClient;
+import com.pawith.authapplication.service.command.feign.response.NaverUserInfo;
+import com.pawith.commonmodule.UnitTestConfig;
+import com.pawith.commonmodule.enums.Provider;
+import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+
+@UnitTestConfig
+@DisplayName("NaverOAuthHandler 테스트")
+class NaverOAuthHandlerTest {
+
+    @Mock
+    private NaverOAuthFeignClient naverOAuthFeignClient;
+
+    private NaverOAuthHandler naverOAuthHandler;
+
+    @BeforeEach
+    void setUp() {
+        naverOAuthHandler = new NaverOAuthHandler(naverOAuthFeignClient);
+    }
+
+    @Test
+    @DisplayName("isAccessible 테스트")
+    void isAccessible() {
+        // given
+        OAuthRequest mockRequest = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeBuilder(OAuthRequest.class)
+            .set("Provider", Provider.NAVER)
+            .sample();
+        // when
+        final boolean result = naverOAuthHandler.isAccessible(mockRequest);
+        // then
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("handle 테스트")
+    void handle() {
+        // given
+        OAuthRequest mockRequest = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(OAuthRequest.class);
+        NaverUserInfo mockNaverUserInfo = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMeOne(NaverUserInfo.class);
+        given(naverOAuthFeignClient.getNaverUserInfo("Bearer "+mockRequest.getAccessToken())).willReturn(mockNaverUserInfo);
+        // when
+        final OAuthUserInfo result = naverOAuthHandler.handle(mockRequest);
+        // then
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result.getEmail()).isEqualTo(mockNaverUserInfo.getEmail());
+        Assertions.assertThat(result.getUsername()).isEqualTo(mockNaverUserInfo.getNickname());
+    }
+
+
+}
