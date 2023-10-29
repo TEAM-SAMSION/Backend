0bf53598fc11f1c9871ea397779a7b607e91872a
tlarbals824 <simkm0824@naver.com>
2023-10-29 16:20:08 +0900

    PET-146 refactor : FixtureMonkeyUtils의 객체 생성 메소드들에 자바 기본타입 설정 추가

--- Common-Module/src/testFixtures/java/com/pawith/commonmodule/utils/FixtureMonkeyUtils.java
+++ Common-Module/src/testFixtures/java/com/pawith/commonmodule/utils/FixtureMonkeyUtils.java
@@ -1,6 +1,7 @@
 package com.pawith.commonmodule.utils;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.FixtureMonkeyBuilder;
 import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
 import com.navercorp.fixturemonkey.api.introspector.JavaTypeArbitraryGenerator;
@@ -11,28 +12,22 @@ public class FixtureMonkeyUtils {
     private FixtureMonkeyUtils() {
     }
 
-    private static final FixtureMonkey CONSTRUCT_BASED_FIXTURE_MONKEY = FixtureMonkey.builder()
+    private static final FixtureMonkey CONSTRUCT_BASED_FIXTURE_MONKEY = setupJavaType(FixtureMonkey.builder())
         .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
         .defaultNotNull(true)
         .build();
 
-    private static final FixtureMonkey BUILDER_BASED_FIXTURE_MONKEY = FixtureMonkey.builder()
+    private static final FixtureMonkey BUILDER_BASED_FIXTURE_MONKEY = setupJavaType(FixtureMonkey.builder())
         .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
         .defaultNotNull(true)
         .build();
 
-    private static final FixtureMonkey REFLECTION_BASED_FIXTURE_MONKEY = FixtureMonkey.builder()
+    private static final FixtureMonkey REFLECTION_BASED_FIXTURE_MONKEY = setupJavaType(FixtureMonkey.builder())
         .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
         .defaultNotNull(true)
         .build();
 
-    private static final FixtureMonkey JAVA_TYPE_BASED_FIXTURE_MONKEY = FixtureMonkey.builder()
-        .javaTypeArbitraryGenerator(new JavaTypeArbitraryGenerator() {
-            @Override
-            public StringArbitrary strings() {
-                return Arbitraries.strings().alpha().numeric().ofMinLength(1).ofMaxLength(10);
-            }
-        })
+    private static final FixtureMonkey JAVA_TYPE_BASED_FIXTURE_MONKEY = setupJavaType(FixtureMonkey.builder())
         .defaultNotNull(true)
         .build();
 
@@ -53,4 +48,13 @@ public class FixtureMonkeyUtils {
     }
 
 
+    private static FixtureMonkeyBuilder setupJavaType(FixtureMonkeyBuilder builder){
+        return builder
+            .javaTypeArbitraryGenerator(new JavaTypeArbitraryGenerator() {
+                @Override
+                public StringArbitrary strings() {
+                    return Arbitraries.strings().alpha().numeric().ofMinLength(1).ofMaxLength(10);
+                }
+            });
+    }
 }
