ee9b05565dd0b0d04f7da2c85493bbad4567f2b4
tlarbals824 <simkm0824@naver.com>
2023-11-05 16:16:27 +0900

    PET-156 feat : 패밀리 탈퇴 시 탈퇴 가능 여부 검사 로직 추가

--- Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/service/UnregisterUseCase.java
+++ Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/service/UnregisterUseCase.java
@@ -3,6 +3,7 @@ package com.pawith.todoapplication.service;
 import com.pawith.commonmodule.annotation.ApplicationService;
 import com.pawith.tododomain.entity.Register;
 import com.pawith.tododomain.service.RegisterQueryService;
+import com.pawith.tododomain.service.RegisterValidateService;
 import com.pawith.userdomain.entity.User;
 import com.pawith.userdomain.utils.UserUtils;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class UnregisterUseCase {
     private final UserUtils userUtils;
     private final RegisterQueryService registerQueryService;
+    private final RegisterValidateService registerValidateService;
 
     public void unregisterTodoTeam(final Long todoTeamId) {
         final User user = userUtils.getAccessUser();
         final Register register = registerQueryService.findRegisterByTodoTeamIdAndUserId(todoTeamId, user.getId());
+        registerValidateService.validatePresidentRegisterDeletable(register);
         register.unregister();
     }
 }
