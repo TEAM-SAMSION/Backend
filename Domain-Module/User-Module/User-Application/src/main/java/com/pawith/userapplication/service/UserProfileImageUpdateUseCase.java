16e0ec3a3ba54cc745dd4844f421108e9ee23ba2
tlarbals824 <simkm0824@naver.com>
2023-11-07 11:00:49 +0900

    PET-159 chore : 이미지 모듈 패키지 경로 수정

--- Domain-Module/User-Module/User-Application/src/main/java/com/pawith/userapplication/service/UserProfileImageUpdateUseCase.java
+++ Domain-Module/User-Module/User-Application/src/main/java/com/pawith/userapplication/service/UserProfileImageUpdateUseCase.java
@@ -1,7 +1,7 @@
 package com.pawith.userapplication.service;
 
 import com.pawith.commonmodule.annotation.ApplicationService;
-import com.pawith.imagedomain.service.ImageUploadService;
+import com.pawith.imagemodule.service.ImageUploadService;
 import com.pawith.userdomain.entity.User;
 import com.pawith.userdomain.utils.UserUtils;
 import lombok.RequiredArgsConstructor;
@@ -17,29 +17,9 @@ public class UserProfileImageUpdateUseCase {
 
     @Transactional
     public void updateUserProfileImage(MultipartFile request) {
-//        uploadImgAsync(request);
         final String imageUrl = imageUploadService.uploadImg(request);
         final User user = userUtils.getAccessUser();
         user.updateProfileImage(imageUrl);
     }
 
-    /**
-     * 비동기로 이미지 업로드, 테스트 좀 해보고 사용할지 생각해봐야할듯
-     */
-//    private CompletableFuture<Void> uploadImgAsync(MultipartFile request) {
-//        return CompletableFuture.runAsync(() ->{
-//            transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-//                @Override
-//                protected void doInTransactionWithoutResult(TransactionStatus status) {
-//                    try {
-//                        final CompletableFuture<String> imageUploadAsync = imageUploadUseCase.uploadImgAsync(request);
-//                        final User user = UserUtils.getAccessUser();
-//                        user.updateProfileImage(imageUploadAsync.join());
-//                    }catch (Exception e) {
-//                        status.setRollbackOnly();
-//                    }
-//                }
-//            });
-//        });
-//    }
 }
