1383f7dead40e236cbd44be602e8fab3fa814e30
tlarbals824 <simkm0824@naver.com>
2023-11-08 17:36:17 +0900

    PET-161 refactor : 소셜 로그인시 사용자 기본 프로필 설정 추가

--- Domain-Module/User-Module/User-Application/src/main/java/com/pawith/userapplication/mapper/UserMapper.java
+++ Domain-Module/User-Module/User-Application/src/main/java/com/pawith/userapplication/mapper/UserMapper.java
@@ -2,19 +2,18 @@ package com.pawith.userapplication.mapper;
 
 import com.pawith.commonmodule.annotation.Mapper;
 import com.pawith.commonmodule.event.UserSignUpEvent;
-import com.pawith.userapplication.dto.request.PathHistoryCreateRequest;
-import com.pawith.userdomain.entity.PathHistory;
 import com.pawith.userdomain.entity.User;
 
 @Mapper
 public class UserMapper {
 
-    public static User toUserEntity(UserSignUpEvent userSignUpEvent){
+    public static User toUserEntity(UserSignUpEvent userSignUpEvent, String imageUrl) {
         return User.builder()
-                .nickname(userSignUpEvent.getNickname())
-                .email(userSignUpEvent.getEmail())
-                .provider(userSignUpEvent.getProvider())
-                .build();
+            .nickname(userSignUpEvent.getNickname())
+            .email(userSignUpEvent.getEmail())
+            .provider(userSignUpEvent.getProvider())
+            .imageUrl(imageUrl)
+            .build();
     }
 
 }
