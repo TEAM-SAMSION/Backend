dc4d75cbe488c6485523173ff1dafcc6f9a23f96
tlarbals824 <simkm0824@naver.com>
2023-11-14 11:08:48 +0900

    PET-144 feat : Todo 알림 엔티티 추가

---
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/entity/TodoNotification.java
@@ -0,0 +1,28 @@
+package com.pawith.tododomain.entity;
+
+import com.pawith.commonmodule.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TodoNotification extends BaseEntity {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_alarm_id")
+    private Long id;
+
+    private LocalTime notificationTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Assign assign;
+
+    @Builder
+    public TodoNotification(LocalTime localTime, Assign assign) {
+        this.notificationTime = localTime;
+        this.assign = assign;
+    }
+}
