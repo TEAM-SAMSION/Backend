084fd9780696bead8a87b45bde452dfb8b32c819
tlarbals824 <simkm0824@naver.com>
2023-11-15 18:53:42 +0900

    PET-144 fix : 기본생성자 추가

--- Alarm-Module/src/main/java/com/pawith/alarmmodule/service/dto/request/DeviceTokenSaveRequest.java
+++ Alarm-Module/src/main/java/com/pawith/alarmmodule/service/dto/request/DeviceTokenSaveRequest.java
@@ -1,10 +1,13 @@
 package com.pawith.alarmmodule.service.dto.request;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DeviceTokenSaveRequest {
     private String deviceToken;
 }
