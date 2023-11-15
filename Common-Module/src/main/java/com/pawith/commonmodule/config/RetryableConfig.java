373486a3f2b3132243767702625e29d70a1fdfa9
tlarbals824 <simkm0824@naver.com>
2023-11-15 18:40:38 +0900

    PET-144 chore : FCM, Retryable, Scheduling 설정 추가

---
+++ Common-Module/src/main/java/com/pawith/commonmodule/config/RetryableConfig.java
@@ -0,0 +1,9 @@
+package com.pawith.commonmodule.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryableConfig {
+}
