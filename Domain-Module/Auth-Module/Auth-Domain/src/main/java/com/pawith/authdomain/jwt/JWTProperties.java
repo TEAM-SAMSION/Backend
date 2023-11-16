6153bf45f2128e620120026af447f67f19581380
tlarbals824 <simkm0824@naver.com>
2023-11-16 14:32:02 +0900

    PET-176 refactor : Spring boot 3.0 버전 변경에 따른 @ConstructorBinding 제거

--- Domain-Module/Auth-Module/Auth-Domain/src/main/java/com/pawith/authdomain/jwt/JWTProperties.java
+++ Domain-Module/Auth-Module/Auth-Domain/src/main/java/com/pawith/authdomain/jwt/JWTProperties.java
@@ -3,11 +3,9 @@ package com.pawith.authdomain.jwt;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
 @RequiredArgsConstructor
-@ConstructorBinding
 @ConfigurationProperties(prefix = "jwt")
 public class JWTProperties {
     private final String secret;
