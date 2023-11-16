0b1ccc7c1e74d7002c41cae2b6f059ee0c1e395a
seeunjang <wisjang1@naver.com>
2023-11-16 13:46:40 +0900

    PET-172 feat : Pet 정보 저장 API 추가

--- Domain-Module/Todo-Module/Todo-Presentation/src/main/java/com/pawith/todopresentation/PetController.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/main/java/com/pawith/todopresentation/PetController.java
@@ -1,21 +1,32 @@
 package com.pawith.todopresentation;
 
 import com.pawith.commonmodule.response.ListResponse;
+import com.pawith.todoapplication.dto.request.PetRegisterRequest;
 import com.pawith.todoapplication.dto.response.PetInfoResponse;
+import com.pawith.todoapplication.service.PetCreateUseCase;
 import com.pawith.todoapplication.service.PetGetUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/teams")
 public class PetController {
+    private final PetCreateUseCase petCreateUseCase;
     private final PetGetUseCase petGetUseCase;
+    @PostMapping("/{todoTeamId}/pet")
+    public void postTodoTeamPet(@PathVariable Long todoTeamId, @RequestPart(value = "petImageFile") MultipartFile petImageFile,
+                                @RequestPart(value = "petInfo") PetRegisterRequest petRegisterRequest) {
+        petCreateUseCase.createPet(todoTeamId, petImageFile, petRegisterRequest);
+    }
 
     @GetMapping("/{todoTeamId}/pet")
     public ListResponse<PetInfoResponse> getTodoTeamPets(@PathVariable Long todoTeamId) {
