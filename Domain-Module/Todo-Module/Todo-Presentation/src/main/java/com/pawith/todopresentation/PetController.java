82bb1919483dde8273434051f2cba09f97db73f9
seeunjang <wisjang1@naver.com>
2023-11-16 14:03:31 +0900

    PET-172 feat : Pet 삭제 API 추가

--- Domain-Module/Todo-Module/Todo-Presentation/src/main/java/com/pawith/todopresentation/PetController.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/main/java/com/pawith/todopresentation/PetController.java
@@ -4,9 +4,11 @@ import com.pawith.commonmodule.response.ListResponse;
 import com.pawith.todoapplication.dto.request.PetRegisterRequest;
 import com.pawith.todoapplication.dto.response.PetInfoResponse;
 import com.pawith.todoapplication.service.PetCreateUseCase;
+import com.pawith.todoapplication.service.PetDeleteUseCase;
 import com.pawith.todoapplication.service.PetGetUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +24,8 @@ import org.springframework.web.multipart.MultipartFile;
 public class PetController {
     private final PetCreateUseCase petCreateUseCase;
     private final PetGetUseCase petGetUseCase;
+    private final PetDeleteUseCase petDeleteUseCase;
+
     @PostMapping("/{todoTeamId}/pet")
     public void postTodoTeamPet(@PathVariable Long todoTeamId, @RequestPart(value = "petImageFile") MultipartFile petImageFile,
                                 @RequestPart(value = "petCreateInfo") PetRegisterRequest petRegisterRequest) {
@@ -32,4 +36,10 @@ public class PetController {
     public ListResponse<PetInfoResponse> getTodoTeamPets(@PathVariable Long todoTeamId) {
         return petGetUseCase.getTodoTeamPets(todoTeamId);
     }
+
+    @DeleteMapping("pet/{petId}")
+    public void deleteTodoTeamPet(@PathVariable Long petId) {
+        petDeleteUseCase.deletePet(petId);
+    }
+
 }
