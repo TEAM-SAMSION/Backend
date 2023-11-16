925fd3f43353081e3401c942ede52b511a26d22e
seeunjang <wisjang1@naver.com>
2023-11-16 14:10:31 +0900

    PET-172 test : Pet 삭제 API 테스트 코드 작성, 명세 반영, Long 타입 JavaTypeBasedFixtureMonkey 통해 생성하도록 수정

--- Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/PetControllerTest.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/PetControllerTest.java
@@ -3,6 +3,7 @@ package com.pawith.todopresentation;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
@@ -19,6 +20,7 @@ import com.pawith.commonmodule.response.ListResponse;
 import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
 import com.pawith.todoapplication.dto.response.PetInfoResponse;
 import com.pawith.todoapplication.service.PetCreateUseCase;
+import com.pawith.todoapplication.service.PetDeleteUseCase;
 import com.pawith.todoapplication.service.PetGetUseCase;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -39,6 +41,8 @@ public class PetControllerTest extends BaseRestDocsTest {
     private PetGetUseCase petGetUseCase;
     @MockBean
     private PetCreateUseCase petCreateUseCase;
+    @MockBean
+    private PetDeleteUseCase petDeleteUseCase;
 
     private static final String PET_REQUEST_URL = "/teams";
     private static final String PET_TEAM_CREATE_INFO = "{\n" +
@@ -53,7 +57,7 @@ public class PetControllerTest extends BaseRestDocsTest {
     @DisplayName("팀의 펫 정보 조회 API 테스트")
     void getTodoTeamPets() throws Exception {
         // given
-        final Long testTeamId = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(Long.class);
+        final Long testTeamId = FixtureMonkeyUtils.getJavaTypeBasedFixtureMonkey().giveMeOne(Long.class);
         final List<PetInfoResponse> petInfoResponses = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMe(PetInfoResponse.class, 2);
         given(petGetUseCase.getTodoTeamPets(testTeamId)).willReturn(ListResponse.from(petInfoResponses));
         MockHttpServletRequestBuilder request = get(PET_REQUEST_URL + "/{todoTeamId}/pet", testTeamId)
@@ -86,7 +90,7 @@ public class PetControllerTest extends BaseRestDocsTest {
     @DisplayName("펫 정보 저장 API 테스트")
     void postTodoTeamPet() throws Exception {
         // given
-        final Long testTeamId = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(Long.class);
+        final Long testTeamId = FixtureMonkeyUtils.getJavaTypeBasedFixtureMonkey().giveMeOne(Long.class);
         final MockMultipartFile mockMultipartFilePet = new MockMultipartFile("petImageFile", "petImageFile", "image/jpeg", "image".getBytes());
         final MockMultipartFile petCreateInfo = new MockMultipartFile("petCreateInfo", "", MediaType.APPLICATION_JSON_VALUE, PET_TEAM_CREATE_INFO.getBytes());
         MockHttpServletRequestBuilder request = multipart(PET_REQUEST_URL + "/{todoTeamId}/pet", testTeamId)
@@ -111,6 +115,27 @@ public class PetControllerTest extends BaseRestDocsTest {
                                 fieldWithPath("petGenus").description("Pet의 종"),
                                 fieldWithPath("petSpecies").description("Pet의 품종"),
                                 fieldWithPath("description").description("Pet의 설명")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("펫 정보 삭제 API 테스트")
+    void deleteTodoTeamPet() throws Exception {
+        // given
+        final Long testPetId = FixtureMonkeyUtils.getJavaTypeBasedFixtureMonkey().giveMeOne(Long.class);
+        MockHttpServletRequestBuilder request = delete(PET_REQUEST_URL + "/pet/{petId}", testPetId)
+                .header("Authorization", "Bearer accessToken");
+        // when
+        ResultActions result = mvc.perform(request);
+        // then
+        result.andExpect(status().isOk())
+                .andDo(resultHandler.document(
+                        requestHeaders(
+                                headerWithName("Authorization").description("access 토큰")
+                        ),
+                        pathParameters(
+                                parameterWithName("petId").description("Pet의 Id")
                         )
                 ));
     }
