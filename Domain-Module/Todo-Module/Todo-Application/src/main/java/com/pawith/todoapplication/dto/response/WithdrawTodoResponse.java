5e963964f9fd905bd817517c92cc0e3a32ca5929
seeunjang <wisjang1@naver.com>
2023-11-16 14:49:05 +0900

    PET-172 refactor : TodoTeam 탈퇴 시 담당했던 Todo들 조회 API 응답값 수정

--- Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/dto/response/WithdrawTodoResponse.java
+++ Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/dto/response/WithdrawTodoResponse.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WithdrawTodoResponse {
+    private Long categoryId;
     private String categoryName;
     private String task;
 }
