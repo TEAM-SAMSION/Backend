30f7d2375f019c12fa07988ff4d264382e85fb5d
seeunjang <wisjang1@naver.com>
2023-11-16 14:13:36 +0900

    PET-172 test : Pet 정보 수정 API 테스트 코드 작성, 명세 반영

--- Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/PetControllerTest.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/PetControllerTest.java
@@ -19,6 +19,7 @@ import com.pawith.commonmodule.BaseRestDocsTest;
 import com.pawith.commonmodule.response.ListResponse;
 import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
 import com.pawith.todoapplication.dto.response.PetInfoResponse;
+import com.pawith.todoapplication.service.PetChangeUseCase;
 import com.pawith.todoapplication.service.PetCreateUseCase;
 import com.pawith.todoapplication.service.PetDeleteUseCase;
 import com.pawith.todoapplication.service.PetGetUseCase;
@@ -43,6 +44,8 @@ public class PetControllerTest extends BaseRestDocsTest {
     private PetCreateUseCase petCreateUseCase;
     @MockBean
     private PetDeleteUseCase petDeleteUseCase;
+    @MockBean
+    private PetChangeUseCase petChangeUseCase;
 
     private static final String PET_REQUEST_URL = "/teams";
     private static final String PET_TEAM_CREATE_INFO = "{\n" +
@@ -136,6 +139,39 @@ public class PetControllerTest extends BaseRestDocsTest {
                         ),
                         pathParameters(
                                 parameterWithName("petId").description("Pet의 Id")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("펫 정보 수정 API 테스트")
+    void putTodoTeamPet() throws Exception {
+        // given
+        final Long testPetId = FixtureMonkeyUtils.getJavaTypeBasedFixtureMonkey().giveMeOne(Long.class);
+        final MockMultipartFile mockMultipartFilePet = new MockMultipartFile("petImageFile", "petImageFile", "image/jpeg", "image".getBytes());
+        final MockMultipartFile petCreateInfo = new MockMultipartFile("petUpdateInfo", "", MediaType.APPLICATION_JSON_VALUE, PET_TEAM_CREATE_INFO.getBytes());
+        MockHttpServletRequestBuilder request = multipart(PET_REQUEST_URL + "/pet/{petId}", testPetId)
+                .file(mockMultipartFilePet)
+                .file(petCreateInfo)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer accessToken");
+        // when
+        ResultActions result = mvc.perform(request);
+        // then
+        result.andExpect(status().isOk())
+                .andDo(resultHandler.document(
+                        requestHeaders(
+                                headerWithName("Authorization").description("access 토큰")
+                        ),
+                        pathParameters(
+                                parameterWithName("petId").description("Pet의 Id")
+                        ),
+                        requestPartFields("petUpdateInfo",
+                                fieldWithPath("name").description("Pet의 이름"),
+                                fieldWithPath("age").description("Pet의 나이"),
+                                fieldWithPath("petGenus").description("Pet의 종"),
+                                fieldWithPath("petSpecies").description("Pet의 품종"),
+                                fieldWithPath("description").description("Pet의 설명")
                         )
                 ));
     }
