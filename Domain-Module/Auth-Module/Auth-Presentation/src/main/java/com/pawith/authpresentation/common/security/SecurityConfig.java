e8343e5140a4cda8211c0c2b7767593cd319ba97
tlarbals824 <simkm0824@naver.com>
2023-11-16 13:58:45 +0900

    PET-176 refactor : spring security 버전 변경에 따른 설정 변경

--- Domain-Module/Auth-Module/Auth-Presentation/src/main/java/com/pawith/authpresentation/common/security/SecurityConfig.java
+++ Domain-Module/Auth-Module/Auth-Presentation/src/main/java/com/pawith/authpresentation/common/security/SecurityConfig.java
@@ -2,30 +2,33 @@ package com.pawith.authpresentation.common.security;
 
 import com.pawith.authpresentation.common.security.filter.JWTAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain chain(HttpSecurity http) throws Exception {
-        http.cors().disable();
+        http.cors(AbstractHttpConfigurer::disable);
         http.addFilterBefore(SecurityHandler.getJWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(SecurityHandler.getJWTEntryPoint(), JWTAuthenticationFilter.class);
 
-        http.csrf().disable();
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        http.authorizeHttpRequests( request ->{
+        http.authorizeHttpRequests(request ->{
             request.anyRequest().permitAll();
         });
 
-        http.formLogin().disable();
-        http.httpBasic().disable();
-        http.logout().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.logout(AbstractHttpConfigurer::disable);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
