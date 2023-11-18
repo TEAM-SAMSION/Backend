286dab615cd703173fe8c123931e0f999f65f9d2
seeunjang <wisjang1@naver.com>
2023-11-18 21:32:53 +0900

    PET-177 refactor : 엔티티 필드 수정 메서드 null 처리 추가

--- Common-Module/src/main/java/com/pawith/commonmodule/util/DomainFieldUtils.java
+++ Common-Module/src/main/java/com/pawith/commonmodule/util/DomainFieldUtils.java
@@ -1,11 +1,12 @@
 package com.pawith.commonmodule.util;
 
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DomainFieldUtils {
     public static <T> T updateIfDifferent(T newValue, T currentValue) {
-        return newValue.equals(currentValue) ? currentValue : newValue;
+        return Objects.equals(newValue, currentValue) ? currentValue : newValue;
     }
 }
