1b8515ea30c69111789600c8aef59c17ee9cad5e
tlarbals824 <simkm0824@naver.com>
2023-11-21 10:59:20 +0900

    PET-186 feat : 카테고리 하위 todo dto 변환 mapper 메소드 추가

--- Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/mapper/TodoMapper.java
+++ Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/mapper/TodoMapper.java
@@ -1,10 +1,15 @@
 package com.pawith.todoapplication.mapper;
 
 import com.pawith.todoapplication.dto.request.TodoCreateRequest;
+import com.pawith.todoapplication.dto.response.AssignUserInfoResponse;
+import com.pawith.todoapplication.dto.response.CategorySubTodoResponse;
+import com.pawith.todoapplication.dto.response.TodoNotificationInfoResponse;
 import com.pawith.tododomain.entity.Category;
 import com.pawith.tododomain.entity.Todo;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TodoMapper {
@@ -14,6 +19,20 @@ public class TodoMapper {
             .category(category)
             .description(request.getDescription())
             .scheduledDate(request.getScheduledDate())
+            .build();
+    }
+
+    public static CategorySubTodoResponse mapToCategorySubTodoResponse(Todo todo,
+                                                                       List<AssignUserInfoResponse> assignsList,
+                                                                       Boolean isAssigned,
+                                                                       TodoNotificationInfoResponse todoNotificationInfoResponse){
+        return CategorySubTodoResponse.builder()
+            .todoId(todo.getId())
+            .task(todo.getDescription())
+            .completionStatus(todo.getCompletionStatus())
+            .assignNames(assignsList)
+            .isAssigned(isAssigned)
+            .notificationInfo(todoNotificationInfoResponse)
             .build();
     }
 }
