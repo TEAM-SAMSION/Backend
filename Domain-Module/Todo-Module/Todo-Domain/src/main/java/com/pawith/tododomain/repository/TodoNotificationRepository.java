011ab442ff3cea1ee43180735a9d93e7ab1b6392
tlarbals824 <simkm0824@naver.com>
2023-11-26 17:08:14 +0900

    PET-201 refactor : 알림 기준 시간 수정

--- Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/TodoNotificationRepository.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/TodoNotificationRepository.java
@@ -8,20 +8,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface TodoNotificationRepository extends JpaRepository<TodoNotification, Long> {
 
-    @Query("select c.todoTeam.id as todoTeamId ,r.userId as userId, c.name as categoryName, t.description as todoDescription, tn.notificationTime as notificationTime " +
-        "from TodoNotification tn " +
-        "join fetch Assign a on tn.assign = a and a.completionStatus='INCOMPLETE' " +
-        "join fetch Todo t on a.todo = t and t.scheduledDate = date(now()) " +
-        "join fetch Category c on t.category = c "+
-        "join fetch Register r on a.register = r and r.isRegistered = true "+
-        "where (tn.notificationTime-current_time) <= :criterionTime " +
-        "and timediff(tn.notificationTime, current_time) > 0")
-    Slice<NotificationDao> findAllWithNotCompletedAssignAndTodayScheduledTodo(Duration criterionTime, Pageable pageable);
+    @Query(value =
+        """
+        select c.todoTeam.id as todoTeamId ,r.userId as userId, c.name as categoryName, t.description as todoDescription, tn.notificationTime as notificationTime
+        from TodoNotification tn
+            join Assign a on tn.assign = a and a.completionStatus='INCOMPLETE'
+            join Todo t on a.todo = t and t.scheduledDate = date(now())
+            join Category c on t.category = c
+            join Register r on a.register = r and r.isRegistered = true
+        where timediff(tn.notificationTime, :alarmTime) <= :criterionTime
+        and timediff(tn.notificationTime , :alarmTime) > 0
+"""
+    )
+    Slice<NotificationDao> findAllWithNotCompletedAssignAndTodayScheduledTodo(Duration criterionTime, LocalTime alarmTime, Pageable pageable);
 
     @Query(value = """
         select tn
