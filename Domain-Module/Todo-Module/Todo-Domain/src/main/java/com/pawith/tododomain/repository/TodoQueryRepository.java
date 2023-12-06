bd8bfd90a261f249a8cfafc6c246c5318f37d101
seeunjang <wisjang1@naver.com>
2023-12-06 20:13:53 +0900

    PET-214 refactor : Todo 쿼리 QueryDSL로 구현

--- Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/TodoQueryRepository.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/repository/TodoQueryRepository.java
@@ -4,7 +4,18 @@ import com.pawith.tododomain.entity.Todo;
 
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface TodoQueryRepository {
     List<Todo> findTodoListByCategoryIdAndScheduledDateQuery(Long categoryId, LocalDate moveDate);
+    Long countTodoByDateQuery(Long userId, Long todoTeamId, LocalDate scheduledDate);
+    Long countCompleteTodoByDateQuery(Long userId, Long todoTeamId, LocalDate scheduledDate);
+    Long countTodoByBetweenDateQuery(Long userId, Long todoTeamId, LocalDate startDate, LocalDate endDate);
+    Long countCompleteTodoByBetweenDateQuery(Long userId, Long todoTeamId, LocalDate startDate, LocalDate endDate);
+    void deleteAllByCategoryIdQuery(Long categoryId);
+    Slice<Todo> findTodoSliceByUserIdAndTodoTeamIdQuery(Long userId, Long todoTeamId, Pageable pageable);
+    Slice<Todo> findTodoSliceByUserIdQuery(Long userId, Pageable pageable);
+    Integer countTodoByUserIdAndTodoTeamIdQuery(Long userId, Long todoTeamId);
+    Integer countTodoByUserIdQuery(Long userId);
 }
--- Domain-Module/Todo-Module/Todo-Infrastructure/src/main/java/com/pawith/todoinfrastructure/repository/TodoRepositoryImpl.java
+++ Domain-Module/Todo-Module/Todo-Infrastructure/src/main/java/com/pawith/todoinfrastructure/repository/TodoRepositoryImpl.java
@@ -1,10 +1,19 @@
 package com.pawith.todoinfrastructure.repository;
 
+import com.pawith.tododomain.entity.CategoryStatus;
+import com.pawith.tododomain.entity.CompletionStatus;
+import com.pawith.tododomain.entity.QAssign;
+import com.pawith.tododomain.entity.QCategory;
+import com.pawith.tododomain.entity.QRegister;
 import com.pawith.tododomain.entity.QTodo;
+import com.pawith.tododomain.entity.QTodoTeam;
 import com.pawith.tododomain.entity.Todo;
 import com.pawith.tododomain.repository.TodoQueryRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -23,4 +32,147 @@ public class TodoRepositoryImpl implements TodoQueryRepository {
                 .and(todo.scheduledDate.eq(moveDate)))
             .fetch();
     }
+
+    @Override
+    public Long countTodoByDateQuery(Long userId, Long todoTeamId, LocalDate scheduledDate) {
+        QTodo todo = QTodo.todo;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        QCategory category = QCategory.category;
+        return jpaQueryFactory.select(todo.count())
+                .from(todo)
+                .join(register).on(register.userId.eq(userId).and(register.todoTeam.id.eq(todoTeamId)))
+                .join(assign).on(assign.register.id.eq(register.id).and(assign.todo.eq(todo)
+                        .and(assign.isDeleted.eq(false))))
+                .join(category).on(category.eq(todo.category).and(category.categoryStatus.eq(CategoryStatus.ON)))
+                .where(todo.scheduledDate.eq(scheduledDate))
+                .fetchOne();
+    }
+
+    @Override
+    public Long countCompleteTodoByDateQuery(Long userId, Long todoTeamId, LocalDate scheduledDate) {
+        QTodo todo = QTodo.todo;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        QCategory category = QCategory.category;
+        return jpaQueryFactory.select(todo.count())
+                .from(todo)
+                .join(register).on(register.userId.eq(userId).and(register.todoTeam.id.eq(todoTeamId)))
+                .join(assign).on(assign.register.id.eq(register.id).and(assign.completionStatus.eq(CompletionStatus.COMPLETE))
+                        .and(assign.todo.eq(todo).and(assign.isDeleted.eq(false))))
+                .join(category).on(category.eq(todo.category).and(category.categoryStatus.eq(CategoryStatus.ON)))
+                .where(todo.scheduledDate.eq(scheduledDate))
+                .fetchOne();
+    }
+
+    @Override
+    public Long countTodoByBetweenDateQuery(Long userId, Long todoTeamId, LocalDate startDate, LocalDate endDate) {
+        QTodo todo = QTodo.todo;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        return jpaQueryFactory.select(todo.count())
+                .from(todo)
+                .join(register).on(register.userId.eq(userId).and(register.todoTeam.id.eq(todoTeamId)))
+                .join(assign).on(assign.register.id.eq(register.id))
+                .where(todo.scheduledDate.between(startDate, endDate).and(assign.todo.eq(todo)))
+                .fetchOne();
+    }
+
+    @Override
+    public Long countCompleteTodoByBetweenDateQuery(Long userId, Long todoTeamId, LocalDate startDate, LocalDate endDate) {
+        QTodo todo = QTodo.todo;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        return jpaQueryFactory.select(todo.count())
+                .from(todo)
+                .join(register).on(register.userId.eq(userId).and(register.todoTeam.id.eq(todoTeamId)))
+                .join(assign).on(assign.register.id.eq(register.id))
+                .where(todo.scheduledDate.between(startDate, endDate).and(assign.todo.eq(todo)).and(todo.completionStatus.eq(CompletionStatus.COMPLETE)))
+                .fetchOne();
+    }
+
+    @Override
+    public void deleteAllByCategoryIdQuery(Long categoryId) {
+        QTodo todo = QTodo.todo;
+        jpaQueryFactory
+                .update(todo)
+                .set(todo.isDeleted, true)
+                .where(todo.category.id.eq(categoryId))
+                .execute();
+    }
+
+    @Override
+    public Slice<Todo> findTodoSliceByUserIdAndTodoTeamIdQuery(Long userId, Long todoTeamId, Pageable pageable) {
+        QTodo todo = QTodo.todo;
+        QCategory category = todo.category;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        List<Todo> todos =  jpaQueryFactory.select(todo)
+                .from(todo)
+                .join(category).fetchJoin()
+                .join(register).on(register.userId.eq(userId).and(register.todoTeam.id.eq(todoTeamId)))
+                .join(assign).on(assign.register.id.eq(register.id))
+                .where(assign.todo.eq(todo))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+        return getSliceImpl(todos, pageable);
+    }
+
+    @Override
+    public Slice<Todo> findTodoSliceByUserIdQuery(Long userId, Pageable pageable) {
+        QTodo todo = QTodo.todo;
+        QCategory category = todo.category;
+        QTodoTeam todoTeam = category.todoTeam;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        List<Todo> todos = jpaQueryFactory.select(todo)
+                .from(todo)
+                .join(category).fetchJoin()
+                .join(todoTeam).fetchJoin()
+                .join(register).on(register.userId.eq(userId))
+                .join(assign).on(assign.register.id.eq(register.id))
+                .where(assign.todo.eq(todo))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+        return getSliceImpl(todos, pageable);
+    }
+
+    @Override
+    public Integer countTodoByUserIdAndTodoTeamIdQuery(Long userId, Long todoTeamId) {
+        QTodo todo = QTodo.todo;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        return jpaQueryFactory.select(todo.count())
+                .from(todo)
+                .join(register).on(register.userId.eq(userId).and(register.todoTeam.id.eq(todoTeamId)))
+                .join(assign).on(assign.register.id.eq(register.id))
+                .where(assign.todo.eq(todo))
+                .fetchOne().intValue();
+    }
+
+    @Override
+    public Integer countTodoByUserIdQuery(Long userId) {
+        QTodo todo = QTodo.todo;
+        QRegister register = QRegister.register;
+        QAssign assign = QAssign.assign;
+        return jpaQueryFactory.select(todo.count())
+                .from(todo)
+                .join(register).on(register.userId.eq(userId))
+                .join(assign).on(assign.register.id.eq(register.id))
+                .where(assign.todo.eq(todo))
+                .fetchOne().intValue();
+    }
+
+    private <T> Slice<T> getSliceImpl(List<T> list, Pageable pageable) {
+        boolean hasNext = false;
+        if (list.size() > pageable.getPageSize()) {
+            hasNext = true;
+            list.remove(list.size() - 1);
+        }
+
+        return new SliceImpl<>(list, pageable, hasNext);
+    }
+
 }
