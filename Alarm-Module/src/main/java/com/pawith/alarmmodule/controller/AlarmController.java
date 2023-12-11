0de1a1d38b21c1af41680cbb86e9793f7a6811a9
tlarbals824 <simkm0824@naver.com>
2023-12-11 20:44:59 +0900

    PET-233 feat : 알림 일괄 조회 api 추가

--- Alarm-Module/src/main/java/com/pawith/alarmmodule/controller/AlarmController.java
+++ Alarm-Module/src/main/java/com/pawith/alarmmodule/controller/AlarmController.java
@@ -1,12 +1,13 @@
 package com.pawith.alarmmodule.controller;
 
 import com.pawith.alarmmodule.service.AlarmService;
-import com.pawith.alarmmodule.service.dto.response.AlarmExistenceResponse;
+import com.pawith.alarmmodule.service.dto.response.UnReadAlarmResponse;
 import com.pawith.alarmmodule.service.dto.response.AlarmInfoResponse;
 import com.pawith.commonmodule.response.SliceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,13 +16,18 @@ public class AlarmController {
     private final AlarmService alarmService;
 
     @GetMapping("/alarms/exist")
-    public AlarmExistenceResponse getAlarmsExist(){
-        return alarmService.getAlarmsExist();
+    public UnReadAlarmResponse getAlarmsExist(){
+        return alarmService.getUnreadAlarmCount();
     }
 
     @GetMapping("/alarms")
     public SliceResponse<AlarmInfoResponse> getAlarms(Pageable pageable){
         return alarmService.getAlarms(pageable);
+    }
+
+    @PatchMapping("/alarms")
+    public void patchAlarms(){
+        alarmService.changeAllAlarmStatusToRead();
     }
 
 }
--- Alarm-Module/src/main/java/com/pawith/alarmmodule/repository/AlarmRepository.java
+++ Alarm-Module/src/main/java/com/pawith/alarmmodule/repository/AlarmRepository.java
@@ -4,15 +4,18 @@ import com.pawith.alarmmodule.entity.Alarm;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
-    @Query("select count(a) > 0 " +
-        "from Alarm a " +
-        "join AlarmUser au on au.userId = :userId and a.alarmUser = au " +
-        "where a.isRead = false")
-    Boolean existsByUserId(Long userId);
+    @Query("""
+        select count(a)
+        from Alarm a
+        join AlarmUser au on au.userId = :userId and a.alarmUser = au
+        where a.isRead = false
+        """)
+    Integer countUnReadAlarm(Long userId);
 
     @Query(
         """
@@ -23,4 +26,13 @@ public interface AlarmRepository extends JpaRepository<Alarm, Long> {
             """
     )
     Slice<Alarm> findAllByUserId(Long userId, Pageable pageable);
+
+
+    @Modifying
+    @Query("""
+            update Alarm a
+            set a.isRead = true
+            where a.isRead = false and a.alarmUser in (select au from AlarmUser au where au.userId = :userId)
+        """)
+    void changeAllAlarmStatusToRead(Long userId);
 }
