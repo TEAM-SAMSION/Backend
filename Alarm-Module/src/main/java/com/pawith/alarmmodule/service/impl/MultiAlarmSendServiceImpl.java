98abbdd299b254c7ad0200c19fe9e0ef6829a126
tlarbals824 <simkm0824@naver.com>
2023-12-18 20:50:27 +0900

    PET-236 feat : 알림 데이터 batch 단위로 처리하는 MultiAlarmSendService 인터페이스 정의 및 구현

---
+++ Alarm-Module/src/main/java/com/pawith/alarmmodule/service/impl/MultiAlarmSendServiceImpl.java
@@ -0,0 +1,55 @@
+package com.pawith.alarmmodule.service.impl;
+
+import com.google.firebase.messaging.Notification;
+import com.pawith.alarmmodule.entity.Alarm;
+import com.pawith.alarmmodule.entity.AlarmUser;
+import com.pawith.alarmmodule.entity.vo.AlarmBody;
+import com.pawith.alarmmodule.fcm.FcmSendMessageHandler;
+import com.pawith.alarmmodule.handler.NotificationHandler;
+import com.pawith.alarmmodule.service.AlarmService;
+import com.pawith.alarmmodule.service.AlarmUserService;
+import com.pawith.alarmmodule.service.MultiAlarmSendService;
+import com.pawith.commonmodule.event.MultiNotificationEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class MultiAlarmSendServiceImpl implements MultiAlarmSendService<MultiNotificationEvent> {
+    private final AlarmUserService alarmUserService;
+    private final AlarmService alarmService;
+    private final NotificationHandler<FcmSendMessageHandler.NotificationInfo> fcmSendHandler;
+
+    @Override
+    @EventListener
+    public void sendAlarm(MultiNotificationEvent multiNotificationEvent) {
+        final Map<Long, AlarmUser> alarmUserMap = alarmUserService.findAlarmUsersByUserIds(multiNotificationEvent.extractAllUserIds());
+        saveAllAlarm(multiNotificationEvent, alarmUserMap);
+        sendMultiNotification(multiNotificationEvent, alarmUserMap);
+    }
+
+    private void sendMultiNotification(MultiNotificationEvent multiNotificationEvent, Map<Long, AlarmUser> alarmUserMap) {
+        final List<FcmSendMessageHandler.NotificationInfo> notificationInfoList = multiNotificationEvent.notificationEvents().stream()
+            .map(notificationEvent -> {
+                final String deviceToken = alarmUserMap.get(notificationEvent.getUserId()).getDeviceToken();
+                final Notification notification = notificationEvent.toNotification();
+                return new FcmSendMessageHandler.NotificationInfo(deviceToken, notification);
+            }).toList();
+        fcmSendHandler.sendMultiAsync(notificationInfoList);
+    }
+
+    private void saveAllAlarm(MultiNotificationEvent multiNotificationEvent, Map<Long, AlarmUser> alarmUserMap) {
+        final List<Alarm> savedAlarmList = multiNotificationEvent.notificationEvents().stream()
+            .map(notificationEvent -> Alarm.builder()
+                .alarmCategory(notificationEvent.getTitle())
+                .alarmUser(alarmUserMap.get(notificationEvent.getUserId()))
+                .alarmBody(new AlarmBody(notificationEvent.getBody(), notificationEvent.getDomainId()))
+                .build())
+            .toList();
+        alarmService.saveAllAlarm(savedAlarmList);
+    }
+}
