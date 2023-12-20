444dd9837260c227141a4fd9b7ce328410401654
seeunjang <wisjang1@naver.com>
2023-12-20 22:19:21 +0900

    PET-255 test : 투두 삭제 및 수정 검증 API 명세 수정

--- Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/dto/response/TodoValidateResponse.java
+++ Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/dto/response/TodoValidateResponse.java
@@ -1,12 +1,15 @@
 package com.pawith.todoapplication.dto.response;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TodoValidateResponse {
-    private boolean isNotValidate;
+    private Boolean isNotValidate;
 }
