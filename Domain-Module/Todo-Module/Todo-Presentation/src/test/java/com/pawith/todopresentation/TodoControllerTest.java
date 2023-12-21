2c5f420b8129bcfe46375a2f8e962bbb1bcabbe9
seeunjang <wisjang1@naver.com>
2023-12-21 20:03:59 +0900

    PET-260 test : Todo 담당자 수정 API 테스트 코드 작성, 명세 추가

--- Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/TodoControllerTest.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/TodoControllerTest.java
@@ -4,6 +4,7 @@ import com.pawith.commonmodule.BaseRestDocsTest;
 import com.pawith.commonmodule.response.ListResponse;
 import com.pawith.commonmodule.response.SliceResponse;
 import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
+import com.pawith.todoapplication.dto.request.AssignChangeRequest;
 import com.pawith.todoapplication.dto.request.ScheduledDateChangeRequest;
 import com.pawith.todoapplication.dto.request.TodoCreateRequest;
 import com.pawith.todoapplication.dto.request.TodoDescriptionChangeRequest;
@@ -527,4 +528,30 @@ public class TodoControllerTest extends BaseRestDocsTest {
                 ));
     }
 
+    @Test
+    @DisplayName("투두 담당자 변경 API 테스트")
+    void changeAssign() throws Exception {
+        //given
+        final Long testTodoId = FixtureMonkeyUtils.getJavaTypeBasedFixtureMonkey().giveMeOne(Long.class);
+        final AssignChangeRequest assignChangeRequest = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMeOne(AssignChangeRequest.class);
+        MockHttpServletRequestBuilder request = put(TODO_REQUEST_URL + "/todos/{todoId}/assign", testTodoId)
+                .content(objectMapper.writeValueAsString(assignChangeRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer accessToken");
+        //when
+        ResultActions result = mvc.perform(request);
+        //then
+        result.andExpect(status().isOk())
+            .andDo(resultHandler.document(
+                requestHeaders(
+                    headerWithName("Authorization").description("access 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("todoId").description("투두 항목 Id")
+                ),
+                requestFields(
+                    fieldWithPath("registerIds[]").description("변경할 담당자들의 registerId")
+                )
+            ));
+    }
 }
