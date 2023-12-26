9a3d53b6c0d49879f84e54a7af0e38953120860a
tlarbals824 <simkm0824@naver.com>
2023-12-26 17:10:08 +0900

    PET-280 refactor : Key(todo) : Value(List<Assign>) 형태의 맵 반환 메소드 추가

--- Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/service/AssignQueryService.java
+++ Domain-Module/Todo-Module/Todo-Domain/src/main/java/com/pawith/tododomain/service/AssignQueryService.java
@@ -1,15 +1,21 @@
 package com.pawith.tododomain.service;
 
 import com.pawith.commonmodule.annotation.DomainService;
+import com.pawith.tododomain.consts.TodoDomainCacheValueConsts;
 import com.pawith.tododomain.entity.Assign;
+import com.pawith.tododomain.entity.Todo;
 import com.pawith.tododomain.exception.AssignNotFoundException;
 import com.pawith.tododomain.exception.TodoError;
 import com.pawith.tododomain.repository.AssignRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @DomainService
 @Transactional(readOnly = true)
@@ -18,8 +24,15 @@ public class AssignQueryService {
 
     private final AssignRepository assignRepository;
 
+    @Cacheable(value = TodoDomainCacheValueConsts.ASSIGN_CACHE_WITH_CATEGORY_ID, key = "#categoryId")
     public List<Assign> findAllAssignByCategoryIdAndScheduledDate(Long categoryId, LocalDate scheduledDate) {
         return assignRepository.findAllByCategoryIdAndScheduledDateQuery(categoryId, scheduledDate);
+    }
+
+    public Map<Todo, List<Assign>> findMapWithTodoKeyAndAssignListValueByCategoryIdAndScheduledDate(Long categoryId, LocalDate scheduledDate) {
+        return assignRepository.findAllByCategoryIdAndScheduledDateQuery(categoryId, scheduledDate)
+            .stream()
+            .collect(Collectors.groupingBy(Assign::getTodo, LinkedHashMap::new, Collectors.toList()));
     }
 
     public List<Assign> findAllByUserIdAndTodoTeamIdAndScheduledDate(Long userId, Long todoTeamId) {
