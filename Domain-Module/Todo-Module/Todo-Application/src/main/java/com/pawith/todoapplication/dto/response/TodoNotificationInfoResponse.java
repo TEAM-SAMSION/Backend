5bf7b53a989a5f481f03b5356629a199d707dddc
tlarbals824 <simkm0824@naver.com>
2023-12-26 17:07:45 +0900

    PET-280 refactor : TodoNotificationInfoResponse record로 변경 및 인스턴스 생성 메소드 변경

--- Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/dto/response/TodoNotificationInfoResponse.java
+++ Domain-Module/Todo-Module/Todo-Application/src/main/java/com/pawith/todoapplication/dto/response/TodoNotificationInfoResponse.java
@@ -1,15 +1,19 @@
 package com.pawith.todoapplication.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import com.pawith.tododomain.entity.TodoNotification;
 
 import java.time.LocalTime;
+import java.util.Optional;
+import java.util.function.Supplier;
 
-@AllArgsConstructor
-@Getter
-public class TodoNotificationInfoResponse {
-    private Boolean isNotification;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private LocalTime notificationTime;
+public record TodoNotificationInfoResponse(
+    Boolean isNotification,
+    @JsonInclude(JsonInclude.Include.NON_NULL) LocalTime notificationTime
+) {
+    public static TodoNotificationInfoResponse of(Supplier<Optional<TodoNotification>> todoNotificationSupplier) {
+        return todoNotificationSupplier.get()
+            .map(todoNotification -> new TodoNotificationInfoResponse(true, todoNotification.getNotificationTime()))
+            .orElse(new TodoNotificationInfoResponse(false, null));
+    }
 }
