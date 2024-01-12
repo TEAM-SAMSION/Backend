cf6b588ea81ebc0b416c4ee773a7a4ecd049fc1d
seeunjang <wisjang1@naver.com>
2024-01-12 20:00:30 +0900

    PET-257 test : ValidateResponse 생성 방식 수정

--- Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/TodoControllerTest.java
+++ Domain-Module/Todo-Module/Todo-Presentation/src/test/java/com/pawith/todopresentation/TodoControllerTest.java
@@ -349,7 +349,7 @@ public class TodoControllerTest extends BaseRestDocsTest {
         //given
         final Long testTodoTeamId = FixtureMonkeyUtils.getJavaTypeBasedFixtureMonkey().giveMeOne(Long.class);
         final Long testTodoId = FixtureMonkeyUtils.getJavaTypeBasedFixtureMonkey().giveMeOne(Long.class);
-        final ValidateResponse validateResponse = FixtureMonkeyUtils.getReflectionbasedFixtureMonkey().giveMeOne(ValidateResponse.class);
+        final ValidateResponse validateResponse = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(ValidateResponse.class);
 
         given(todoValidationUseCase.validateDeleteAndUpdateTodoByTodoId(any(), any())).willReturn(validateResponse);
         MockHttpServletRequestBuilder request = get(TODO_REQUEST_URL + "/{todoTeamId}/todos/{todoId}/validate", testTodoTeamId, testTodoId)
