466857a64d50d3381921946e3a86e70a97259eae
tlarbals824 <simkm0824@naver.com>
2024-02-12 16:08:32 +0900

    PET-302 feat : 소셜 로그인 성공 이벤트 처리 클래스 추가 및 테스트 추가

---
+++ Domain-Module/Auth-Module/Auth-Application/src/main/java/com/pawith/authapplication/handler/OAuthSuccessHandler.java
@@ -0,0 +1,52 @@
+package com.pawith.authapplication.handler;
+
+import com.pawith.authapplication.handler.request.OAuthSuccessEvent;
+import com.pawith.authdomain.entity.OAuth;
+import com.pawith.authdomain.exception.AuthError;
+import com.pawith.authdomain.exception.OAuthException;
+import com.pawith.authdomain.service.OAuthQueryService;
+import com.pawith.authdomain.service.OAuthSaveService;
+import com.pawith.commonmodule.event.UserSignUpEvent;
+import com.pawith.userdomain.entity.User;
+import com.pawith.userdomain.service.UserQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuthSuccessHandler {
+    private final OAuthQueryService oAuthQueryService;
+    private final OAuthSaveService oAuthSaveService;
+    private final UserQueryService userQueryService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @EventListener(OAuthSuccessEvent.class)
+    @Transactional
+    public void handle(OAuthSuccessEvent oAuthSuccessEvent) {
+        if (oAuthQueryService.existBySub(oAuthSuccessEvent.sub())) {
+            final OAuth oAuth = oAuthQueryService.findBySub(oAuthSuccessEvent.sub());
+            final User user = userQueryService.findById(oAuth.getUserId());
+            user.updateEmail(oAuthSuccessEvent.email());
+        } else {
+            if(userQueryService.checkEmailAlreadyExist(oAuthSuccessEvent.email())) {
+                throw new OAuthException(AuthError.INVALID_OAUTH_REQUEST);
+            }
+            applicationEventPublisher.publishEvent(UserSignUpEvent.of(
+                oAuthSuccessEvent.username(),
+                oAuthSuccessEvent.email()
+            ));
+            final User user = userQueryService.findByEmail(oAuthSuccessEvent.email());
+            OAuth oAuth = OAuth.of(
+                oAuthSuccessEvent.provider(),
+                oAuthSuccessEvent.sub(),
+                user.getId()
+            );
+            oAuthSaveService.save(oAuth);
+        }
+    }
+}
---
+++ Domain-Module/Auth-Module/Auth-Application/src/test/java/com/pawith/authapplication/handler/OAuthSuccessHandlerTest.java
@@ -0,0 +1,95 @@
+package com.pawith.authapplication.handler;
+
+import com.pawith.authapplication.handler.request.OAuthSuccessEvent;
+import com.pawith.authdomain.entity.OAuth;
+import com.pawith.authdomain.exception.OAuthException;
+import com.pawith.authdomain.service.OAuthQueryService;
+import com.pawith.authdomain.service.OAuthSaveService;
+import com.pawith.commonmodule.UnitTestConfig;
+import com.pawith.commonmodule.event.UserSignUpEvent;
+import com.pawith.commonmodule.utils.FixtureMonkeyUtils;
+import com.pawith.userdomain.entity.User;
+import com.pawith.userdomain.service.UserQueryService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.context.ApplicationEventPublisher;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@UnitTestConfig
+@DisplayName("OAuthSuccessHandler 테스트")
+class OAuthSuccessHandlerTest {
+
+    @Mock
+    private OAuthQueryService oAuthQueryService;
+    @Mock
+    private OAuthSaveService oAuthSaveService;
+    @Mock
+    private UserQueryService userQueryService;
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    private OAuthSuccessHandler oAuthSuccessHandler;
+
+    @BeforeEach
+    void setUp() {
+        oAuthSuccessHandler = new OAuthSuccessHandler(oAuthQueryService, oAuthSaveService, userQueryService, applicationEventPublisher);
+    }
+
+    @Test
+    @DisplayName("소셜 로그인 성공 이벤트를 처리할 때, 기존 요청 sub에 대해 OAuth가 존재하면 User의 email을 업데이트한다.")
+    void handleWithExistOAuth() {
+        // given
+        final OAuthSuccessEvent oAuthSuccessEvent = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(OAuthSuccessEvent.class);
+        final User user = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(User.class);
+        final OAuth oAuth = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeBuilder(OAuth.class)
+            .set("userId", user.getId())
+            .sample();
+        given(oAuthQueryService.existBySub(oAuthSuccessEvent.sub())).willReturn(true);
+        given(oAuthQueryService.findBySub(oAuthSuccessEvent.sub())).willReturn(oAuth);
+        given(userQueryService.findById(oAuth.getUserId())).willReturn(user);
+        // when
+        oAuthSuccessHandler.handle(oAuthSuccessEvent);
+        // then
+        Assertions.assertThat(user.getEmail()).isEqualTo(oAuthSuccessEvent.email());
+    }
+
+    @Nested
+    @DisplayName("소셜 로그인 성공 이벤트를 처리할 때, 기존 요청 sub에 대해 OAuth가 존재하지 않으면")
+    class Describe_handleWithNotExistOAuth {
+        @Test
+        @DisplayName("User의 email이 이미 존재하면, OAuthException을 발생시킨다.")
+        void handleWithExistEmail() {
+            // given
+            final OAuthSuccessEvent oAuthSuccessEvent = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(OAuthSuccessEvent.class);
+            given(oAuthQueryService.existBySub(oAuthSuccessEvent.sub())).willReturn(false);
+            given(userQueryService.checkEmailAlreadyExist(oAuthSuccessEvent.email())).willReturn(true);
+            // when
+            // then
+            Assertions.assertThatCode(() -> oAuthSuccessHandler.handle(oAuthSuccessEvent))
+                .isInstanceOf(OAuthException.class);
+        }
+
+        @Test
+        @DisplayName("User의 email이 존재하지 않으면,  User와 OAuth 정보를 저장한다.")
+        void handleWithNotExistEmail() {
+            // given
+            final OAuthSuccessEvent oAuthSuccessEvent = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(OAuthSuccessEvent.class);
+            final User user = FixtureMonkeyUtils.getConstructBasedFixtureMonkey().giveMeOne(User.class);
+            given(oAuthQueryService.existBySub(oAuthSuccessEvent.sub())).willReturn(false);
+            given(userQueryService.checkEmailAlreadyExist(oAuthSuccessEvent.email())).willReturn(false);
+            given(userQueryService.findByEmail(oAuthSuccessEvent.email())).willReturn(user);
+            // when
+            oAuthSuccessHandler.handle(oAuthSuccessEvent);
+            // then
+            then(applicationEventPublisher).should().publishEvent(any(UserSignUpEvent.class));
+            then(oAuthSaveService).should().save(any(OAuth.class));
+        }
+    }
+}
