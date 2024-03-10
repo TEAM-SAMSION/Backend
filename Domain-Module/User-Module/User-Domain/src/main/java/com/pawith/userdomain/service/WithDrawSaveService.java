ff0e81848519915cff9449493bede52e6662e6be
Sim-km <simkm0824@naver.com>
2024-03-10 19:22:55 +0900

    PET-310 fix : 도메인 서비스 트랜잭션 제거

--- Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/service/WithDrawSaveService.java
+++ Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/service/WithDrawSaveService.java
@@ -3,14 +3,12 @@ package com.pawith.userdomain.service;
 import com.pawith.commonmodule.annotation.DomainService;
 import com.pawith.userdomain.entity.WithdrawReason;
 import com.pawith.userdomain.repository.WithdrawReasonRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @DomainService
 @RequiredArgsConstructor
 public class WithDrawSaveService {
     private final WithdrawReasonRepository withdrawReasonRepository;
-    @Transactional
     public void save(WithdrawReason reason) {
         withdrawReasonRepository.save(reason);
     }
